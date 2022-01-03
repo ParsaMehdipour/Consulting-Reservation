2b8a9d64a89499d8a271e4d5ab3ac112db819488
Parsa <Mehdipourparsa@gmail.com>
2022-01-03 14:59:34 +0330

    افزودن لوگو و عکس بک گراند 1400/10/13

--- CR.Presentation/wwwroot/assets/js/profile-settings.js
+++ CR.Presentation/wwwroot/assets/js/profile-settings.js
@@ -35,19 +35,19 @@ Version      : 1.0
 					'<div class="col-12 col-md-6 col-lg-4">' +
 						'<div class="form-group">' +
 							'<label>مدرک</label>' +
-							'<input type="text" class="form-control">' +
+							'<input type="text" class="form-control degreeOfEducation" id="degreeOfEducation" name="degreeOfEducation">' +
 						'</div>' +
 					'</div>' +
 					'<div class="col-12 col-md-6 col-lg-4">' +
 						'<div class="form-group">' +
 							'<label>دانشگاه/موسسه</label>' +
-							'<input type="text" class="form-control">' +
+							'<input type="text" class="form-control university" id="university" name="university">' +
 						'</div>' +
 					'</div>' +
 					'<div class="col-12 col-md-6 col-lg-4">' +
 						'<div class="form-group">' +
 							'<label>سال اتمام</label>' +
-							'<input type="text" class="form-control">' +
+							'<input type="text" class="form-control endDate" id="endDate" name="endDate">' +
 						'</div>' +
 					'</div>' +
 				'</div>' +
