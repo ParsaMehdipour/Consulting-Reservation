fef43dbb8e1a7dc9b86edaadbd3e519d35dd130b
Parsa <Mehdipourparsa@gmail.com>
2022-01-16 15:33:56 +0330

    مدل جهت نمایش تمام کارمزد و تخفیف ها در پنل ادمین 1400/10/26

---
+++ CR.Core/DTOs/ResultDTOs/ResultGetALLCommissionAndDiscountsForAdminDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CR.Core.DTOs.CommissionAndDiscounts;
+using CR.DataAccess.Entities.CommissionAndDiscounts;
+
+namespace CR.Core.DTOs.ResultDTOs
+{
+    public class ResultGetALLCommissionAndDiscountsForAdminDto
+    {
+        public List<CommissionAndDiscountForAdminDto> CommissionAndDiscountForAdminDtos { get; set; }
+    }
+}
