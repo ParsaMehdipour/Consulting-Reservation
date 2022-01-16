fce7bde7cf6e272842403c0538b7e3661eb2e17b
Parsa <Mehdipourparsa@gmail.com>
2022-01-16 15:33:30 +0330

    پیاده سازی سرویس افزودن کارمزد و تخفیف جدید 1400/10/26

---
+++ CR.Core/DTOs/RequestDTOs/RequestAddNewCommissionAndDiscountDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CR.Core.DTOs.RequestDTOs
+{
+    public class RequestAddNewCommissionAndDiscountDto
+    {
+        public long expertInformationId { get; set; }
+        public string phoneCallCommission { get; set; } = "0";
+        public string voiceCallCommission { get; set; } = "0";
+        public string textCallCommission { get; set; } = "0";
+        public string phoneCallDiscount { get; set; } = "0";
+        public string voiceCallDiscount { get; set; } = "0";
+        public string textCallDiscount { get; set; } = "0";
+    }
+}
