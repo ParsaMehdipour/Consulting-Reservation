ea076d5edba6d3be37e792940bd3046f273739fb
Parsa <Mehdipourparsa@gmail.com>
2022-01-16 15:29:56 +0330

    migration 1400/10/26

--- CR.DataAccess/Migrations/20220116075320_AddedCommissionAndDiscount.Designer.cs
+++ CR.DataAccess/Migrations/20220116075320_AddedCommissionAndDiscount.Designer.cs
@@ -4,14 +4,16 @@ using CR.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CR.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220116075320_AddedCommissionAndDiscount")]
+    partial class AddedCommissionAndDiscount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
