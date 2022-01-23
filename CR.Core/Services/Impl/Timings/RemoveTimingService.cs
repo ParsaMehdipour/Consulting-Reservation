c138a48a45f27c977da5b0207a0704ea8612be7b
Parsa <Mehdipourparsa@gmail.com>
2022-01-23 12:37:19 +0330

    پیاده سازی سرویس حذف زمانبندی 1400/11/03

---
+++ CR.Core/Services/Impl/Timings/RemoveTimingService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using CR.Common.DTOs;
+using CR.Core.Services.Interfaces.Timings;
+using CR.DataAccess.Context;
+
+namespace CR.Core.Services.Impl.Timings
+{
+    public class RemoveTimingService : IRemoveTimingService
+    {
+        private readonly ApplicationContext _context;
+
+        public RemoveTimingService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto Execute(long id)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                var timing = _context.Timings.FirstOrDefault(t => t.Id == id);
+
+                if (timing == null)
+                {
+                    return new ResultDto()
+                    {
+                        IsSuccess = false,
+                        Message = "زمانبندی یافت نشد!!"
+                    };
+                }
+
+                _context.Timings.Remove(timing);
+
+                _context.SaveChanges();
+
+                transaction.Commit();
+
+                return new ResultDto()
+                {
+                    IsSuccess = true,
+                    Message = "زمانبندی با موفقیت حذف شد"
+                };
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+
+                return new ResultDto()
+                {
+                    IsSuccess = false,
+                    Message = "خطا از سمت سرور!!"
+                };
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+    }
+}
---
+++ CR.Core/Services/Interfaces/Timings/IRemoveTimingService.cs
@@ -0,0 +1,9 @@
+﻿using CR.Common.DTOs;
+
+namespace CR.Core.Services.Interfaces.Timings
+{
+    public interface IRemoveTimingService
+    {
+        ResultDto Execute(long id);
+    }
+}
