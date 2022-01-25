a6f0f8a71c74b7962020768c978fbfdaf0918272
Parsa <Mehdipourparsa@gmail.com>
2022-01-25 14:59:16 +0330

    ایجاد سرویس و نمایش زمان های یک متخصص بر اساس نوع زمانبندی انتخابی 1400/11/05

---
+++ CR.Presentation/Controllers/Api/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.ExpertAvailabilities;
+using CR.Core.DTOs.RequestDTOs.Timing;
+using CR.Core.Services.Interfaces.ExpertAvailabilities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CR.Presentation.Controllers.Api
+{
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IGetExpertAvailabilitiesForReservationService _getExpertAvailabilitiesForReservationService;
+
+        public ReservationController(IGetExpertAvailabilitiesForReservationService getExpertAvailabilitiesForReservationService)
+        {
+            _getExpertAvailabilitiesForReservationService = getExpertAvailabilitiesForReservationService;
+        }
+
+        [Route("/api/Reservation/GetDetails")]
+        [HttpPost]
+        public ResultDto<List<DayDto>> GetDetails(RequestGetSpecificTiming model)
+        {
+            var outPut = _getExpertAvailabilitiesForReservationService.Execute(model.expertInformationId, model.timingType);
+
+            return outPut;
+        }
+
+    }
+}
