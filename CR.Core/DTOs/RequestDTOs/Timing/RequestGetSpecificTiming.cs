9e65e45fb9f681ee951e8a2eeeab87bec8e5e6ff
Parsa <Mehdipourparsa@gmail.com>
2022-01-25 15:01:18 +0330

    پیاده سازی سرویس آوردن زمانبندی بر اساس نوع زمانبندی 1400/11/05

---
+++ CR.Core/DTOs/RequestDTOs/Timing/RequestGetSpecificTiming.cs
@@ -0,0 +1,10 @@
+﻿using CR.DataAccess.Enums;
+
+namespace CR.Core.DTOs.RequestDTOs.Timing
+{
+    public class RequestGetSpecificTiming
+    {
+        public TimingType timingType { get; set; }
+        public long expertInformationId { get; set; }
+    }
+}
---
+++ CR.Core/Services/Interfaces/ExpertAvailabilities/IGetExpertAvailabilitiesForReservationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CR.Common.DTOs;
+using CR.Core.DTOs.ExpertAvailabilities;
+using CR.DataAccess.Enums;
+
+namespace CR.Core.Services.Interfaces.ExpertAvailabilities
+{
+    public interface IGetExpertAvailabilitiesForReservationService
+    {
+        ResultDto<List<DayDto>> Execute(long expertInformationId , TimingType timingType);
+    }
+}
