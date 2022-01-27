e7a7ca585454e86de3427f3074a7c6d4f20e745e
Parsa <Mehdipourparsa@gmail.com>
2022-01-27 13:54:14 +0330

    تغیر در مدل افزودن زمانبندی چدید 1400/11/07

--- CR.Core/DTOs/RequestDTOs/RequestAddAppointmentDto.cs
+++ CR.Core/DTOs/RequestDTOs/RequestAddAppointmentDto.cs
@@ -1,9 +1,11 @@
-﻿namespace CR.Core.DTOs.RequestDTOs
+﻿using CR.DataAccess.Enums;
+
+namespace CR.Core.DTOs.RequestDTOs
 {
     public class RequestAddAppointmentDto
     {
         public long timeOfDayId { get; set; }
         public long expertInformationId { get; set; }
-        public long consumerId { get; set; }
+        public CallingType callingType { get; set; }
     }
 }
