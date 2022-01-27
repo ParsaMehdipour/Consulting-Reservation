e888fad1fa0e83b16267fb19bda7cd2e9d489f1e
Parsa <Mehdipourparsa@gmail.com>
2022-01-27 13:51:58 +0330

    تغیر در مدل اطلاعات متخصص در صفحه رزرو 1400/11/07

--- CR.Core/DTOs/Experts/ExpertDetailsForReservationDto.cs
+++ CR.Core/DTOs/Experts/ExpertDetailsForReservationDto.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using CR.Core.DTOs.ExpertAvailabilities;
+using System;
 using System.Collections.Generic;
-using CR.Core.DTOs.ExpertAvailabilities;
 
 namespace CR.Core.DTOs.Experts
 {
@@ -10,9 +10,6 @@ namespace CR.Core.DTOs.Experts
         public string Speciality { get; set; }
         public string SpecialitySrc { get; set; }
         public string FullName { get; set; }
-        public bool usePhoneCall { get; set; }
-        public bool useVoiceCall { get; set; }
-        public bool useTextCall { get; set; }
         public long? phonePrice { get; set; }
         public long? voicePrice { get; set; }
         public long? textPrice { get; set; }
