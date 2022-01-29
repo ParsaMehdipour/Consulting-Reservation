fe1e5ab5221c6e1e87161f5accf50ee5b93b89e8
Parsa <Mehdipourparsa@gmail.com>
2022-01-29 18:52:25 +0330

    تغیر در paymentcontroller 1400/11/09

--- CR.Presentation/Controllers/View/PaymentController.cs
+++ CR.Presentation/Controllers/View/PaymentController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CR.Core.Services.Interfaces.Factors;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CR.Presentation.Controllers.View
 {
     public class PaymentController : Controller
     {
+        private readonly IGetFactorDetailsService _getFactorDetailsService;
+
+        public PaymentController(IGetFactorDetailsService getFactorDetailsService)
+        {
+            _getFactorDetailsService = getFactorDetailsService;
+        }
+
         public IActionResult Index(string factorNumber)
         {
-            return View();
+            var model = _getFactorDetailsService.Execute(factorNumber).Data;
+
+            return View(model);
         }
     }
 }
