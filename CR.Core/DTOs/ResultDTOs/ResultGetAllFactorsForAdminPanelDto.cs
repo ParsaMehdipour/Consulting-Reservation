cfe68b629f4cca17fb935635a8453fc74eec5b26
Parsa <Mehdipourparsa@gmail.com>
2022-01-29 18:47:38 +0330

    افزودن مدل کلی نمایش فاکتورها در پنل ادمین 1400/11/09

---
+++ CR.Core/DTOs/ResultDTOs/ResultGetAllFactorsForAdminPanelDto.cs
@@ -0,0 +1,13 @@
+﻿using CR.Core.DTOs.Factors;
+using System.Collections.Generic;
+
+namespace CR.Core.DTOs.ResultDTOs
+{
+    public class ResultGetAllFactorsForAdminPanelDto
+    {
+        public int RowCount { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public List<FactorForAdminDto> FactorForAdminDtos { get; set; }
+    }
+}
