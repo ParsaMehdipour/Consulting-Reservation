08478789e30a48b45174d9747b11639db13768d5
Parsa <Mehdipourparsa@gmail.com>
2022-01-29 18:46:38 +0330

    افزودن مدل جهت نمایش فاکتور در پنل ادمین 1400/11/09

---
+++ CR.Core/DTOs/Factors/FactorForAdminDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CR.Core.DTOs.Factors
+{
+    public class FactorForAdminDto
+    {
+        public string FactorNumber { get; set; }
+        public long ConsumerId { get; set; }
+        public string ConsumerIconSrc { get; set; }
+        public string ConsumerFullName { get; set; }
+        public string TotalPrice { get; set; }
+        public string CreateDate { get; set; }
+        public string Status { get; set; }
+    }
+}
