5d07e9d8b62bf5cf61245d226fcf3eb0298ed293
Parsa <Mehdipourparsa@gmail.com>
2022-01-29 18:51:29 +0330

    ایجاد کنترلر جهت نمایش فاکتور ها در پنل ادمین 1400/11/09

---
+++ CR.Presentation/Areas/AdminPanel/Controllers/FactorsController.cs
@@ -0,0 +1,25 @@
+﻿using CR.Core.Services.Interfaces.Factors;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers
+{
+    [Authorize]
+    [Area("AdminPanel")]
+    public class FactorsController : Controller
+    {
+        private readonly IGetAllFactorsForAdminPanelService _getAllFactorsForAdminPanel;
+
+        public FactorsController(IGetAllFactorsForAdminPanelService getAllFactorsForAdminPanel)
+        {
+            _getAllFactorsForAdminPanel = getAllFactorsForAdminPanel;
+        }
+
+        public IActionResult Index(int page = 1, int pageSize = 20)
+        {
+            var model = _getAllFactorsForAdminPanel.Execute(page, pageSize).Data;
+
+            return View(model);
+        }
+    }
+}
