b99d150c7c7deee8ba0936e0f0371dba0df90260
Parsa <Mehdipourparsa@gmail.com>
2022-01-29 18:49:09 +0330

    پیاده سازی سرویس نمایش کل فاکتور ها در پنل ادمین 1400/11/09

---
+++ CR.Core/Services/Impl/Factors/GetAllFactorsForAdminPanelService.cs
@@ -0,0 +1,53 @@
+﻿using CR.Common.Convertor;
+using CR.Common.DTOs;
+using CR.Common.Utilities;
+using CR.Core.DTOs.Factors;
+using CR.Core.DTOs.ResultDTOs;
+using CR.Core.Services.Interfaces.Factors;
+using CR.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CR.Core.Services.Impl.Factors
+{
+    public class GetAllFactorsForAdminPanelService : IGetAllFactorsForAdminPanelService
+    {
+        private readonly ApplicationContext _context;
+
+        public GetAllFactorsForAdminPanelService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<ResultGetAllFactorsForAdminPanelDto> Execute(int Page = 1, int PageSize = 20)
+        {
+            int rowCount = 0;
+
+            var factors = _context.Factors
+                .Include(f => f.Appointments)
+                .Include(f => f.ConsumerInformation)
+                .Select(f => new FactorForAdminDto()
+                {
+                    ConsumerId = f.ConsumerInformation.ConsumerId,
+                    ConsumerFullName = f.ConsumerInformation.FirstName + " " + f.ConsumerInformation.LastName,
+                    ConsumerIconSrc = f.ConsumerInformation.IconSrc,
+                    CreateDate = f.CreateDate.ToShamsi(),
+                    FactorNumber = f.FactorNumber,
+                    Status = f.FactorStatus.GetDisplayName(),
+                    TotalPrice = f.TotalPrice.ToString("n0")
+                }).ToList();
+
+            return new ResultDto<ResultGetAllFactorsForAdminPanelDto>()
+            {
+                Data = new ResultGetAllFactorsForAdminPanelDto()
+                {
+                    FactorForAdminDtos = factors,
+                    CurrentPage = Page,
+                    PageSize = PageSize,
+                    RowCount = rowCount,
+                },
+                IsSuccess = true
+            };
+        }
+    }
+}
---
+++ CR.Core/Services/Interfaces/Factors/IGetAllFactorsForAdminPanelService.cs
@@ -0,0 +1,10 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.ResultDTOs;
+
+namespace CR.Core.Services.Interfaces.Factors
+{
+    public interface IGetAllFactorsForAdminPanelService
+    {
+        ResultDto<ResultGetAllFactorsForAdminPanelDto> Execute(int Page = 1, int PageSize = 20);
+    }
+}
