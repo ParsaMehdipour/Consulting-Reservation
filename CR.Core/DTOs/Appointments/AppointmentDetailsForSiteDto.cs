e0d5b7476e2ace43e8875bdabb0370ae6dbb51e7
Parsa <Mehdipourparsa@gmail.com>
2022-01-29 18:45:42 +0330

    افزودن مد جزییات نوبت برای صفحه پرداخت 1400/11/09

---
+++ CR.Core/DTOs/Appointments/AppointmentDetailsForSiteDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CR.Core.DTOs.Appointments
+{
+    public class AppointmentDetailsForSiteDto
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string TimingType { get; set; }
+        public string CallingType { get; set; }
+        public long Price { get; set; }
+
+    }
+}
