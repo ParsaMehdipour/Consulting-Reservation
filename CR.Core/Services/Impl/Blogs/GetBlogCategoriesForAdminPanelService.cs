bce17654bf82c49a4d2c3451794f45d01af968e5
Parsa <Mehdipourparsa@gmail.com>
2022-01-31 18:46:21 +0330

    ایجاد سرویس و نمایش گروه مقالات در پنل ادمین 1400/11/11

---
+++ CR.Core/Services/Impl/Blogs/GetBlogCategoriesForAdminPanelService.cs
@@ -0,0 +1,63 @@
+﻿using CR.Common.Convertor;
+using CR.Common.DTOs;
+using CR.Core.DTOs.Blogs;
+using CR.Core.DTOs.ResultDTOs;
+using CR.Core.Services.Interfaces.Blogs;
+using CR.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CR.Core.Services.Impl.Blogs
+{
+    public class GetBlogCategoriesForAdminPanelService : IGetBlogCategoriesForAdminPanelService
+    {
+        private readonly ApplicationContext _context;
+
+        public GetBlogCategoriesForAdminPanelService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<ResultGetBlogCategoriesForAdminPanelDto> Execute(long? parentId, int Page = 1, int PageSize = 20)
+        {
+            int rowCount = 0;
+
+            var blogCategories = _context.BlogCategories.Include(b => b.ParentCategory)
+                .Include(b => b.SubCategories)
+                .Where(b => b.ParentCategoryId == parentId)
+                .Select(b => new BlogCategoryForAdminPanelDto
+                {
+                    Id = b.Id,
+                    PictureSrc = b.PictureSrc,
+                    BlogsCount = 0,
+                    CreationDate = b.CreateDate.ToShamsi(),
+                    Description = b.Description,
+                    Name = b.Name,
+                    ShowOrder = b.ShowOrder,
+                    Slug = b.Slug,
+                    ParentCategory = b.ParentCategory != null
+                        ? new
+                            ParentBlogCategoryDto
+                        {
+                            Id = b.ParentCategory.Id,
+                            Name = b.ParentCategory.Name,
+
+                        }
+                        : null,
+                    HasChild = b.SubCategories.Any() ? true : false
+                }).OrderBy(b => b.ShowOrder).ToList();
+
+            return new ResultDto<ResultGetBlogCategoriesForAdminPanelDto>()
+            {
+                Data = new ResultGetBlogCategoriesForAdminPanelDto()
+                {
+                    BlogCategoryForAdminPanelDtos = blogCategories,
+                    CurrentPage = Page,
+                    PageSize = PageSize,
+                    RowCount = rowCount,
+                },
+                IsSuccess = true
+            };
+        }
+    }
+}
---
+++ CR.Presentation/Areas/AdminPanel/Controllers/BlogCategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using CR.Core.Services.Interfaces.Blogs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers
+{
+    [Authorize]
+    [Area("AdminPanel")]
+    public class BlogCategoriesController : Controller
+    {
+        private readonly IGetBlogCategoriesForAdminPanelService _getBlogCategoriesForAdminPanelService;
+
+        public BlogCategoriesController(IGetBlogCategoriesForAdminPanelService getBlogCategoriesForAdminPanelService)
+        {
+            _getBlogCategoriesForAdminPanelService = getBlogCategoriesForAdminPanelService;
+        }
+
+        public IActionResult Index(long? parentId, int page = 1, int pageSize = 20)
+        {
+            var model = _getBlogCategoriesForAdminPanelService.Execute(parentId, page, pageSize).Data;
+
+            return View(model);
+        }
+    }
+}
