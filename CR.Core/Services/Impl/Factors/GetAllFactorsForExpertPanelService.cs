42e87a4a1e5175f254160222f0d2d8c2630b6bf5
Parsa <Mehdipourparsa@gmail.com>
2022-01-31 18:45:04 +0330

    ایجاد سرویس و نمایش فاکتور ها در پنل متخصص 1400/11/11

---
+++ CR.Core/Services/Impl/Factors/GetAllFactorsForExpertPanelService.cs
@@ -0,0 +1,55 @@
+﻿using CR.Common.Convertor;
+using CR.Common.DTOs;
+using CR.Common.Utilities;
+using CR.Core.DTOs.Factors;
+using CR.Core.DTOs.ResultDTOs;
+using CR.Core.Services.Interfaces.Factors;
+using CR.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CR.Core.Services.Impl.Factors
+{
+    public class GetAllFactorsForExpertPanelService : IGetAllFactorsForExpertPanelService
+    {
+        private readonly ApplicationContext _context;
+
+        public GetAllFactorsForExpertPanelService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<ResultGetAllFactorsForExpertPanelService> Execute(long expertId, int Page = 1, int PageSize = 20)
+        {
+            int rowCount = 0;
+
+            var factors = _context.Factors
+                .Include(f => f.ExpertInformation)
+                .Include(f => f.Appointments)
+                .Include(f => f.ConsumerInformation)
+                .Where(f => f.ExpertInformation.ExpertId == expertId)
+                .Select(f => new FactorForExpertPanelDto()
+                {
+                    ConsumerId = f.ConsumerInformation.ConsumerId,
+                    ConsumerFullName = f.ConsumerInformation.FirstName + " " + f.ConsumerInformation.LastName,
+                    ConsumerIconSrc = f.ConsumerInformation.IconSrc,
+                    CreateDate = f.CreateDate.ToShamsi(),
+                    FactorNumber = f.FactorNumber,
+                    Status = f.FactorStatus.GetDisplayName(),
+                    TotalPrice = f.TotalPrice.ToString("n0")
+                }).ToList();
+
+            return new ResultDto<ResultGetAllFactorsForExpertPanelService>()
+            {
+                Data = new ResultGetAllFactorsForExpertPanelService()
+                {
+                    FactorForExpertPanelDtos = factors,
+                    CurrentPage = Page,
+                    PageSize = PageSize,
+                    RowCount = rowCount,
+                },
+                IsSuccess = true
+            };
+        }
+    }
+}
---
+++ CR.Presentation/Areas/ExpertPanel/Controllers/FactorsController.cs
@@ -0,0 +1,28 @@
+﻿using CR.Common.Utilities;
+using CR.Core.Services.Interfaces.Factors;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.ExpertPanel.Controllers
+{
+    [Authorize]
+    [Area("ExpertPanel")]
+    public class FactorsController : Controller
+    {
+        private readonly IGetAllFactorsForExpertPanelService _getAllFactorsForExpertPanelService;
+
+        public FactorsController(IGetAllFactorsForExpertPanelService getAllFactorsForExpertPanelService)
+        {
+            _getAllFactorsForExpertPanelService = getAllFactorsForExpertPanelService;
+        }
+
+        public IActionResult Index(int Page = 1, int PageSize = 20)
+        {
+            var expertId = ClaimUtility.GetUserId(User).Value;
+
+            var model = _getAllFactorsForExpertPanelService.Execute(expertId, Page, PageSize).Data;
+
+            return View(model);
+        }
+    }
+}
