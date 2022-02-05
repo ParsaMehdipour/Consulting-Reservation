63046ff22112152b970055a5829a81050eba3d27
Parsa <Mehdipourparsa@gmail.com>
2022-02-05 11:59:09 +0330

    ایجاد کنترلر های مربوط به گروه مقاله 1400/11/16

---
+++ CR.Presentation/Areas/AdminPanel/Controllers/Api/BlogCategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using CR.Core.DTOs.RequestDTOs.BlogCategories;
+using CR.Core.Services.Interfaces.Blogs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers.Api
+{
+    [ApiController]
+    public class BlogCategoriesController : ControllerBase
+    {
+        private readonly IDeleteBlogCategoryService _deleteBlogCategoryService;
+
+        public BlogCategoriesController(IDeleteBlogCategoryService deleteBlogCategoryService)
+        {
+            _deleteBlogCategoryService = deleteBlogCategoryService;
+        }
+
+        [Route("/api/BlogCategories/DeleteBlogCategory")]
+        [HttpPost]
+        public IActionResult DeleteBlogCategory(RequestDeleteBlogCategory model)
+        {
+            var result = _deleteBlogCategoryService.Execute(model.id);
+
+            return new JsonResult(result);
+        }
+    }
+}
--- CR.Presentation/Areas/AdminPanel/Controllers/BlogCategoriesController.cs
+++ CR.Presentation/Areas/AdminPanel/Controllers/BlogCategoriesController.cs
@@ -1,6 +1,9 @@
-﻿using CR.Core.Services.Interfaces.Blogs;
+﻿using CR.Core.DTOs.RequestDTOs;
+using CR.Core.Services.Interfaces.Blogs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
 namespace CR.Presentation.Areas.AdminPanel.Controllers
 {
@@ -9,17 +12,67 @@ namespace CR.Presentation.Areas.AdminPanel.Controllers
     public class BlogCategoriesController : Controller
     {
         private readonly IGetBlogCategoriesForAdminPanelService _getBlogCategoriesForAdminPanelService;
+        private readonly IAddNewBlogCategoryService _addNewBlogCategoryService;
+        private readonly IGetBlogCategoryDetailsService _getBlogCategoryDetailsService;
+        private readonly IEditBlogCategoryDetailsService _editBlogCategoryDetailsService;
 
-        public BlogCategoriesController(IGetBlogCategoriesForAdminPanelService getBlogCategoriesForAdminPanelService)
+        public BlogCategoriesController(IGetBlogCategoriesForAdminPanelService getBlogCategoriesForAdminPanelService
+        , IAddNewBlogCategoryService addNewBlogCategoryService
+        , IGetBlogCategoryDetailsService getBlogCategoryDetailsService
+        , IEditBlogCategoryDetailsService editBlogCategoryDetailsService)
         {
             _getBlogCategoriesForAdminPanelService = getBlogCategoriesForAdminPanelService;
+            _addNewBlogCategoryService = addNewBlogCategoryService;
+            _getBlogCategoryDetailsService = getBlogCategoryDetailsService;
+            _editBlogCategoryDetailsService = editBlogCategoryDetailsService;
         }
 
         public IActionResult Index(long? parentId, int page = 1, int pageSize = 20)
         {
-            var model = _getBlogCategoriesForAdminPanelService.Execute(parentId, page, pageSize).Data;
+            var model = _getBlogCategoriesForAdminPanelService.Execute(parentId, page, pageSize).Data.BlogCategoryForAdminPanelDtos;
 
-            return View(model);
+            ViewData["parentId"] = parentId;
+
+            var parentBlogCategory = model.Select(p => new
+            {
+                Id = p.Id,
+                Name = p.Name
+            });
+
+            ViewBag.parent = new SelectList(parentBlogCategory, "Id", "Name");
+
+            return View(_getBlogCategoriesForAdminPanelService.Execute(parentId).Data);
         }
+
+        public IActionResult AddNewBlogCategory(long? parentId)
+        {
+            ViewBag.parentId = parentId;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNewBlogCategory(RequestAddNewBlogCategoryDto model)
+        {
+            var result = _addNewBlogCategoryService.Execute(model);
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet]
+        public IActionResult Details(long id)
+        {
+            var details = _getBlogCategoryDetailsService.Execute(id).Data;
+
+            return View(details);
+        }
+
+        [HttpPost]
+        public IActionResult EditBlogCategory(RequestEditBlogCategoryDto request)
+        {
+            var result = _editBlogCategoryDetailsService.Execute(request);
+
+            return new JsonResult(result);
+        }
+
     }
 }
