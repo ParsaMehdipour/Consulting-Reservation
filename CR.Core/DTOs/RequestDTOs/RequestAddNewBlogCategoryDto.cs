08da0330d11a47f6d113bd604869059758ac465b
Parsa <Mehdipourparsa@gmail.com>
2022-02-05 11:55:04 +0330

    پیاده سازی سرویس افزودن گروه مقاله جدید 1400/11/16

---
+++ CR.Core/DTOs/RequestDTOs/RequestAddNewBlogCategoryDto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace CR.Core.DTOs.RequestDTOs
+{
+    public class RequestAddNewBlogCategoryDto
+    {
+        public long? parentId { get; set; }
+
+        [Required(ErrorMessage = "عنوان گروه مقاله را وارد کنید")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Slug را وارد کنید")]
+        public string slug { get; set; }
+
+        [Required(ErrorMessage = "ترتیب نمایش را وارد کنید")]
+        public int showOrder { get; set; }
+
+        public string description { get; set; }
+
+        public string metaDescription { get; set; }
+
+        public IFormFile file { get; set; }
+    }
+}
