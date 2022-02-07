ee928249c95cf596c93d0cae52065a1d023cf105
Parsa <Mehdipourparsa@gmail.com>
2022-02-07 11:29:01 +0330

    کانفیگ مدل عای گروه مقاله و مقاله 1400/11/18

---
+++ CR.DataAccess/Configurations/BlogConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CR.DataAccess.Entities.Blogs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CR.DataAccess.Configurations
+{
+    public class BlogConfiguration : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            builder.ToTable("TBL_Blogs");
+
+            builder.HasOne(_ => _.BlogCategory)
+                .WithMany(_ => _.Blogs)
+                .HasForeignKey(_ => _.BlogCategoryId);
+        }
+    }
+}
