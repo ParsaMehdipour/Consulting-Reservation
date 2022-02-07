5ebbd0071c67ff8489fd95847ecd8d1f407a1564
Parsa <Mehdipourparsa@gmail.com>
2022-02-07 14:56:23 +0330

    پیاده سازی سرویس بروزرسانی رفرنس آیدی فاکتور 1400/11.18

---
+++ CR.Core/Services/Interfaces/Factors/IUpdateFactorRefIdService.cs
@@ -0,0 +1,9 @@
+﻿using CR.Common.DTOs;
+
+namespace CR.Core.Services.Interfaces.Factors
+{
+    public interface IUpdateFactorRefIdService
+    {
+        ResultDto Execute(string factorNumber, string refId);
+    }
+}
