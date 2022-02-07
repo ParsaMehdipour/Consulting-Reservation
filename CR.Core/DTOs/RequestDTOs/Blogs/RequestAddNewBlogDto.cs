68e8607abd21b1b280452155ab9311eb44dfb5ae
Parsa <Mehdipourparsa@gmail.com>
2022-02-07 14:54:20 +0330

    رفع باگ های جاوا اسکریپتی ویو افزودن بلاگ 1400/11/18

--- CR.Core/DTOs/RequestDTOs/Blogs/RequestAddNewBlogDto.cs
+++ CR.Core/DTOs/RequestDTOs/Blogs/RequestAddNewBlogDto.cs
@@ -1,17 +1,36 @@
-﻿namespace CR.Core.DTOs.RequestDTOs.Blogs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CR.Core.DTOs.RequestDTOs.Blogs
 {
     public class RequestAddNewBlogDto
     {
+        [Required(ErrorMessage = "عنوان مقاله را وارد کنید")]
         public string title { get; set; }
+
         public string slug { get; set; }
+
+        [Required(ErrorMessage = "گروه مقاله را وارد کنید")]
         public long blogCategoryId { get; set; }
-        public long shortDescription { get; set; }
+
+        [Required(ErrorMessage = "توضیحات کوتاه را وارد کنید")]
+        public string shortDescription { get; set; }
+
+        [Required(ErrorMessage = "توضیحات را وارد کنید")]
         public string description { get; set; }
+
+        [Required(ErrorMessage = "ترتیب نمایش را وارد کنید")]
         public int orderNumber { get; set; }
+
         public string keyWords { get; set; }
+
         public bool status { get; set; }
+
+        [Required(ErrorMessage = "توضیحات متا را وارد کنید")]
         public string metaDescription { get; set; }
+
         public string canonicalAddress { get; set; }
+
+        [Required(ErrorMessage = "تاریخ انتشار را وارد کنید")]
         public string publishDate { get; set; }
     }
 }
---
+++ CR.Presentation/Areas/AdminPanel/Controllers/Api/BlogsController.cs
@@ -0,0 +1,26 @@
+﻿using CR.Core.DTOs.RequestDTOs.Blogs;
+using CR.Core.Services.Interfaces.Blogs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers.Api
+{
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly IAddNewBlogService _addNewBlogService;
+
+        public BlogsController(IAddNewBlogService addNewBlogService)
+        {
+            _addNewBlogService = addNewBlogService;
+        }
+
+        [Route("/api/Blogs/AddNewBlog")]
+        [HttpPost]
+        public IActionResult AddNewBlog([FromBody] RequestAddNewBlogDto model)
+        {
+            var result = _addNewBlogService.Execute(model);
+
+            return new JsonResult(result);
+        }
+    }
+}
