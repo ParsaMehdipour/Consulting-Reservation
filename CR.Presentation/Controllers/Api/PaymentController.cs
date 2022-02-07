db62339bc7938c2aec365938314d3ca733783994
Parsa <Mehdipourparsa@gmail.com>
2022-02-07 14:57:10 +0330

    تغیر در کنترلر های پرداخت 1400/11/18

--- CR.Presentation/Controllers/Api/PaymentController.cs
+++ CR.Presentation/Controllers/Api/PaymentController.cs
@@ -1,5 +1,6 @@
 ﻿using CR.Common.DTOs;
 using CR.Core.DTOs.RequestDTOs.Payment;
+using CR.Core.Services.Interfaces.Factors;
 using Microsoft.AspNetCore.Mvc;
 using ServiceReference2;
 using System;
@@ -10,10 +11,18 @@ namespace CR.Presentation.Controllers.Api
     [ApiController]
     public class PaymentController : ControllerBase
     {
+        private readonly IUpdateFactorRefIdService _updateFactorRefIdService;
+
+        public PaymentController(IUpdateFactorRefIdService updateFactorRefIdService)
+        {
+            _updateFactorRefIdService = updateFactorRefIdService;
+        }
+
         [Route("/api/Payment/RedirectToPayment")]
         [HttpPost]
         public IActionResult RedirectToPayment(RedirectToPaymentDto model)
         {
+
             if (model.price == 0 || model.factorNumber == 0)
             {
                 return new JsonResult(new ResultDto()
@@ -22,40 +31,6 @@ namespace CR.Presentation.Controllers.Api
                     Message = "شماره فاکتور یا مبلغ قابل پرداخت نامعتبر است"
                 });
             }
-
-            //var date = DateTime.Now;
-
-            //var request = new RequestPaymentDto()
-            //{
-            //    amount = Convert.ToInt32(price),
-            //    callBackUrl = "http://localhost:23065/Payment/Verify",
-            //    orderId = Convert.ToInt32(factorNumber),
-            //    terminalId = 6240330,
-            //    userName = "choole777",
-            //    userPassword = "16020307",
-            //    localDate = $"{date.Year}{date.Month}{date.Day}",
-            //    localTime = $"{date.Hour}{date.Minute}{date.Second}",
-            //    payerId = ClaimUtility.GetUserId(User).Value,
-            //    additionalData = "پس از نیم ساعت امکان لغو درخواست وجود ندارد"
-            //};
-
-            //var data = new
-            //{
-            //    request.amount,
-            //    request.callBackUrl,
-            //    request.terminalId,
-            //    request.orderId,
-            //    request.userName,
-            //    request.userPassword,
-            //    request.localDate,
-            //    request.localTime,
-            //    request.payerId,
-            //    request.additionalData
-            //};
-
-            //var ipgUrl = "https://bpm.shaparak.ir/pgwchannel/services/pgw?wsdl";
-            //var res = CallApi<object>(ipgUrl, data);
-            //res.Wait();
 
             var res = CallApi(model.price.ToString(), model.factorNumber.ToString());
             res.Wait();
@@ -69,18 +44,13 @@ namespace CR.Presentation.Controllers.Api
 
                 if (status == "0")
                 {
+                    _updateFactorRefIdService.Execute(model.factorNumber.ToString(), refId);
                     return new RedirectResult("https://pgw.bpm.bankmellat.ir/pgwchannel/startpay.mellat?RefId=" + refId);
                 }
             }
 
             return new RedirectResult("/");
 
-        }
-
-        [Route("/api/Payment/Verify")]
-        public IActionResult Verify(string refId, long saleOrderId)
-        {
-            return new JsonResult('s');
         }
 
         private async Task<bpPayRequestResponse> CallApi(string price, string factorNumber)
