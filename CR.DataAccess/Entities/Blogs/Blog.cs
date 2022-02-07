4ca6354d8a5dd53694f01d413d01937533e4d0e7
Parsa <Mehdipourparsa@gmail.com>
2022-02-07 11:29:41 +0330

    افزودن مدل مقاله 1400/11/18

--- CR.DataAccess/Entities/Blogs/Blog.cs
+++ CR.DataAccess/Entities/Blogs/Blog.cs
@@ -1,8 +1,33 @@
 ﻿using CR.DataAccess.Common.Entity;
+using System;
 
 namespace CR.DataAccess.Entities.Blogs
 {
     public class Blog : BaseEntity
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+        public int ShowOrder { get; set; }
+        public string Slug { get; set; }
+        public string Keywords { get; set; }
+        public bool Status { get; set; }
+        public string MetaDescription { get; set; }
+        public string CanonicalAddress { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        #region Foreign Keys
+
+        public long UserId { get; set; }
+        public long BlogCategoryId { get; set; }
+
+        #endregion
+
+        #region Navigation Properties
+
+        public BlogCategory BlogCategory { get; set; }
+
+        #endregion
+
     }
 }
