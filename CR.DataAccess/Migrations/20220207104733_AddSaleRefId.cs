a153a00a7076485b9da7dd02fb1056eb4589047e
Parsa <Mehdipourparsa@gmail.com>
2022-02-07 14:53:36 +0330

    Migrations 1400/11/18

---
+++ CR.DataAccess/Migrations/20220207104733_AddSaleRefId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class AddSaleRefId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "SaleReferenceId",
+                table: "TBL_Factors",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SaleReferenceId",
+                table: "TBL_Factors");
+        }
+    }
+}
