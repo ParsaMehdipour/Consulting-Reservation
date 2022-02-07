39c7533a61ca685def8770bff6b66806ad87108f
Parsa <Mehdipourparsa@gmail.com>
2022-02-07 11:27:14 +0330

    ایجاد سرویس آوردن گروه مقالات برای دراپ داون 1400/11/18

---
+++ CR.Core/Services/Impl/Blogs/GetBlogCategoriesForDropdownService.cs
@@ -0,0 +1,34 @@
+﻿using CR.Core.DTOs.Blogs;
+using CR.Core.Services.Interfaces.Blogs;
+using CR.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CR.Core.Services.Impl.Blogs
+{
+    public class GetBlogCategoriesForDropdownService : IGetBlogCategoriesForDropdownService
+    {
+        private readonly ApplicationContext _context;
+
+        public GetBlogCategoriesForDropdownService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public List<BlogCategoryForDropdownDto> Execute()
+        {
+            var categories = _context.BlogCategories
+                .Include(c => c.ParentCategory)
+                .Where(p => p.ParentCategoryId != null)
+                .OrderBy(c => c.ShowOrder)
+                .Select(c => new BlogCategoryForDropdownDto()
+                {
+                    Id = c.Id,
+                    Name = $"{c.ParentCategory.Name} - {c.Name}",
+                }).ToList();
+
+            return categories;
+        }
+    }
+}
