6a33f5c8589473aa5d749630c819c2c7bd40baf9
Parsa <Mehdipourparsa@gmail.com>
2022-02-08 16:24:39 +0330

    Migrations 1400/11/19

---
+++ CR.DataAccess/Migrations/20220208105244_AddedPictureSrcToBlog.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class AddedPictureSrcToBlog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PictureSrc",
+                table: "TBL_Blogs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureSrc",
+                table: "TBL_Blogs");
+        }
+    }
+}
