74ee87b13b7f62bf8e97b56158dc1324d03696de
Parsa <Mehdipourparsa@gmail.com>
2022-02-08 12:16:54 +0330

    رفع باگ قیمت هر نوبت 1400/11/19

---
+++ CR.DataAccess/Entities/Transactions/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using CR.DataAccess.Common.Entity;
+using CR.DataAccess.Enums;
+
+namespace CR.DataAccess.Entities.Transactions
+{
+    public class Transaction : BaseEntity
+    {
+        public long PayerId { get; set; }
+        public long ReceiverId { get; set; }
+        public long Price_Digit { get; set; }
+        public string Price_String { get; set; }
+        public string CreateDate_String { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public TransactionStatus Status { get; set; }
+
+        #region Navigation Properties
+
+        public long? FactorId { get; set; }
+
+        #endregion
+    }
+}
--- CR.DataAccess/Enums/CustomEnum.cs
+++ CR.DataAccess/Enums/CustomEnum.cs
@@ -60,12 +60,12 @@ namespace CR.DataAccess.Enums
 
     public enum TransactionStatus
     {
-        //[Display(Name = "نامشخص")]
-        //UnDefined = 0,
+        [Display(Name = "نامشخص")]
+        UnDefined = 0,
         [Display(Name = "ناموفق")]
-        Failed = 0,
+        Failed = 1,
         [Display(Name = "موفق")]
-        Successful = 1,
+        Successful = 2,
 
     }
 
