25f1172d62dbe4795a8f8886e8d1ba01c7fa0204
Parsa <Mehdipourparsa@gmail.com>
2022-02-09 14:28:54 +0330

    ایجاد سرویس جهت بروزرسانی جزییات فاکتور به هنگام پرداخت 1400/11/20

---
+++ CR.Core/Services/Impl/Factors/UpdateFactorStatusService.cs
@@ -0,0 +1,75 @@
+﻿using CR.Common.DTOs;
+using CR.Core.Services.Interfaces.Factors;
+using CR.DataAccess.Context;
+using CR.DataAccess.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace CR.Core.Services.Impl.Factors
+{
+    public class UpdateFactorStatusService : IUpdateFactorStatusService
+    {
+        private readonly ApplicationContext _context;
+
+        public UpdateFactorStatusService(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public ResultDto Execute(string factorNumber, FactorStatus factorStatus)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                var factor = _context.Factors
+                    .Include(_ => _.Appointments)
+                    .ThenInclude(_ => _.TimeOfDay)
+                    .First(_ => _.FactorNumber == factorNumber);
+
+                if (factor == null)
+                {
+                    return new ResultDto()
+                    {
+                        IsSuccess = false,
+                        Message = string.Empty
+                    };
+                }
+
+                factor.FactorStatus = factorStatus;
+
+                if (factorStatus == FactorStatus.SuccessfulPayment)
+                {
+                    foreach (var appointment in factor.Appointments)
+                    {
+                        appointment.TimeOfDay.IsReserved = true;
+                    }
+                }
+
+                _context.SaveChanges();
+
+                transaction.Commit();
+
+                return new ResultDto()
+                {
+                    IsSuccess = true,
+                    Message = string.Empty
+                };
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+
+                return new ResultDto()
+                {
+                    IsSuccess = false,
+                    Message = string.Empty
+                };
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+    }
+}
