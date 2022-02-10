e6fec176e197aeba23dabbb8f97e5ca3cf18e4c0
Parsa <Mehdipourparsa@gmail.com>
2022-02-10 13:06:25 +0330

    پیاده سازی مدل تراکنش برای نمایش در پنل ادمین 1400/11/21

---
+++ CR.Core/DTOs/FinancialTransactions/FinancialTransactionForAdminDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CR.Core.DTOs.FinancialTransactions
+{
+    public class FinancialTransactionForAdminDto
+    {
+        public long PayerId { get; set; }
+        public string PayerIconSrc { get; set; }
+        public string PayerFullName { get; set; }
+        public long ReceiverId { get; set; }
+        public string ReceiverIconSrc { get; set; }
+        public string ReceiverFullName { get; set; }
+        public string Price { get; set; }
+        public string CreateDate { get; set; }
+        public string TransactionType { get; set; }
+        public string TransactionStatus { get; set; }
+    }
+}
