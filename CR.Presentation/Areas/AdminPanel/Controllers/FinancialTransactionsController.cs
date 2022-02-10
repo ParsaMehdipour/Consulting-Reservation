01adafd804397d92ca1025de4383ecde856131b0
Parsa <Mehdipourparsa@gmail.com>
2022-02-10 13:07:55 +0330

    ایجاد کنترلر تراکنش ها 1400/11/21

---
+++ CR.Presentation/Areas/AdminPanel/Controllers/FinancialTransactionsController.cs
@@ -0,0 +1,22 @@
+﻿using CR.Core.Services.Interfaces.FinancialTransaction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers
+{
+    public class FinancialTransactionsController : Controller
+    {
+        private readonly IGetFinancialTransactionsForAdminService _getFinancialTransactionsForAdminService;
+
+        public FinancialTransactionsController(IGetFinancialTransactionsForAdminService getFinancialTransactionsForAdminService)
+        {
+            _getFinancialTransactionsForAdminService = getFinancialTransactionsForAdminService;
+        }
+
+        public IActionResult Index(int page = 1, int pageSize = 20)
+        {
+            var model = _getFinancialTransactionsForAdminService.Execute(page, pageSize).Data;
+
+            return View(model);
+        }
+    }
+}
