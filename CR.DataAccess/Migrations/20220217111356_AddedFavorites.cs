286a6c6ab830033b8789d721715abad03ae91707
Parsa <Mehdipourparsa@gmail.com>
2022-02-19 15:14:37 +0330

    Migrations 1400/11/30

---
+++ CR.DataAccess/Migrations/20220217111356_AddedFavorites.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class AddedFavorites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBL_Favorites",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    ExpertInformationId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBL_Favorites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBL_Favorites_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TBL_Favorites_TBL_ExpertInformations_ExpertInformationId",
+                        column: x => x.ExpertInformationId,
+                        principalTable: "TBL_ExpertInformations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBL_Favorites_ExpertInformationId",
+                table: "TBL_Favorites",
+                column: "ExpertInformationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBL_Favorites_UserId",
+                table: "TBL_Favorites",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBL_Favorites");
+        }
+    }
+}
---
+++ CR.DataAccess/Migrations/20220219104629_RenamePostalCodeToDegree.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class RenamePostalCodeToDegree : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PostalCode",
+                table: "TBL_ConsumersInformations",
+                newName: "Degree");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Degree",
+                table: "TBL_ConsumersInformations",
+                newName: "PostalCode");
+        }
+    }
+}
