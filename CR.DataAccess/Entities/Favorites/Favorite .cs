8f34608fdab33d36072fad326ec1570df7f04e6b
Parsa <Mehdipourparsa@gmail.com>
2022-02-19 15:14:56 +0330

    افزودن مدل علاقه مندی 1400/11/30

---
+++ CR.DataAccess/Entities/Favorites/Favorite .cs
@@ -0,0 +1,23 @@
+﻿using CR.DataAccess.Common.Entity;
+using CR.DataAccess.Entities.IndividualInformations;
+using CR.DataAccess.Entities.Users;
+
+namespace CR.DataAccess.Entities.Favorites
+{
+    public class Favorite : BaseEntity
+    {
+        #region Foreign Keys
+
+        public long UserId { get; set; }
+        public long ExpertInformationId { get; set; }
+
+        #endregion
+
+        #region Navigation Properties
+
+        public User User { get; set; }
+        public ExpertInformation ExpertInformation { get; set; }
+
+        #endregion
+    }
+}
