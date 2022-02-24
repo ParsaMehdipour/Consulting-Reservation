b8b9158e37f0dc1c4121a01a5ab17a57ee7e874b
Parsa <Mehdipourparsa@gmail.com>
2022-02-24 17:52:24 +0330

    پیاده سازی سرویس و نمایش لیست نظرات مربوط به مشاورین 1400/12/05

---
+++ CR.Core/DTOs/Comments/Experts/ExpertCommentForAdminPanelDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CR.Core.DTOs.Comments.Experts
+{
+    public class ExpertCommentForAdminPanelDto
+    {
+        public long Id { get; set; }
+        public string CommenterIconSrc { get; set; }
+        public string CommenterFullName { get; set; }
+        public long CommenterId { get; set; }
+        public string ExpertFullName { get; set; }
+        public string ExpertIconSrc { get; set; }
+        public long ExpertId { get; set; }
+        public string Message { get; set; }
+        public string CreateDate { get; set; }
+    }
+}
---
+++ CR.Core/Services/Implementations/Comments/GetExpertCommentsForAdminPanelService.cs
@@ -0,0 +1,62 @@
+﻿using CR.Common.Convertor;
+using CR.Common.DTOs;
+using CR.Common.Utilities;
+using CR.Core.DTOs.Comments.Experts;
+using CR.Core.DTOs.ResultDTOs.Comments;
+using CR.Core.Services.Interfaces.Comments;
+using CR.DataAccess.Context;
+using CR.DataAccess.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace CR.Core.Services.Implementations.Comments
+{
+    public class GetExpertCommentsForAdminPanelService : IGetExpertCommentsForAdminPanelService
+    {
+        private readonly ApplicationContext _context;
+
+        public GetExpertCommentsForAdminPanelService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<ResultGetAllExpertCommentsForAdminPanelDto> Execute(int Page = 1, int PageSize = 20)
+        {
+            var expertInformations = _context.ExpertInformations.AsQueryable();
+
+            var expertComments = _context.Comments
+                .Include(_ => _.User)
+                .Where(_ => _.TypeId == CommentType.Expert)
+                .Select(_ => new ExpertCommentForAdminPanelDto
+                {
+                    CommenterFullName = _.User.FirstName + " " + _.User.LastName,
+                    CommenterIconSrc = _.User.IconSrc,
+                    CommenterId = _.User.Id,
+                    CreateDate = _.CreateDate.ToShamsi(),
+                    ExpertFullName = expertInformations.FirstOrDefault(e => e.Id == _.OwnerRecordId).FirstName + " " +
+                                     expertInformations.FirstOrDefault(e => e.Id == _.OwnerRecordId).LastName,
+                    ExpertId = expertInformations.FirstOrDefault(e => e.Id == _.OwnerRecordId).ExpertId,
+                    ExpertIconSrc = expertInformations.FirstOrDefault(e => e.Id == _.OwnerRecordId).IconSrc,
+                    Id = _.Id,
+                    Message = (_.Message.Length > 15)
+                        ? _.Message.Substring(0, Math.Min(_.Message.Length, 15)) + "..."
+                        : _.Message
+                }).AsEnumerable()
+                .ToPaged(Page, PageSize, out var rowsCount)
+                .ToList();
+
+            return new ResultDto<ResultGetAllExpertCommentsForAdminPanelDto>()
+            {
+                Data = new ResultGetAllExpertCommentsForAdminPanelDto()
+                {
+                    ExpertCommentForAdminPanelDtos = expertComments,
+                    CurrentPage = Page,
+                    PageSize = PageSize,
+                    RowCount = rowsCount
+                },
+                IsSuccess = true
+            };
+        }
+    }
+}
