9ddef3d209a423d24a48fda8cafe67798d5dc5d4
Parsa <Mehdipourparsa@gmail.com>
2022-02-27 12:59:56 +0330

    پیاده سازی سرویس افزودن تراکنش پرداخت از درگاه 1400/12/08

---
+++ CR.Core/DTOs/Payment/RedirectToPaymentForReservationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CR.Core.DTOs.Payment
+{
+    public class RedirectToPaymentForReservationDto
+    {
+        public string transactionNumber { get; set; }
+        public int price { get; set; }
+    }
+}
---
+++ CR.Core/Services/Implementations/FinancialTransactions/AddPaymentTransactionService.cs
@@ -0,0 +1,109 @@
+﻿using CR.Common.Convertor;
+using CR.Common.DTOs;
+using CR.Common.Utilities;
+using CR.Core.DTOs.Payment;
+using CR.Core.Services.Interfaces.FinancialTransaction;
+using CR.DataAccess.Context;
+using CR.DataAccess.Entities.FinancialTransactions;
+using CR.DataAccess.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace CR.Core.Services.Implementations.FinancialTransactions
+{
+    public class AddPaymentTransactionService : IAddPaymentTransactionService
+    {
+        private readonly ApplicationContext _context;
+
+        public AddPaymentTransactionService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<RedirectToPaymentForReservationDto> Execute(long factorId, int price)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                if (factorId == 0 || price == 0)
+                {
+                    return new ResultDto<RedirectToPaymentForReservationDto>()
+                    {
+                        Data = new RedirectToPaymentForReservationDto(),
+                        IsSuccess = false,
+                        Message = "لطفا مبلغ را وارد کنید"
+                    };
+                }
+
+                var factor = _context.Factors.Include(_ => _.ConsumerInformation).FirstOrDefault(_ => _.Id == factorId);
+
+                if (factor == null)
+                {
+                    return new ResultDto<RedirectToPaymentForReservationDto>()
+                    {
+                        Data = new RedirectToPaymentForReservationDto(),
+                        Message = "فاکتور یافت نشد!!",
+                        IsSuccess = false
+                    };
+                }
+
+                var financialTransaction = new FinancialTransaction()
+                {
+                    PayerId = factor.ConsumerInformation.ConsumerId,
+                    Price_Digit = price,
+                    Factor = factor,
+                    FactorId = factor.Id,
+                    CreateDate_String = DateTime.Now.ToShamsi(),
+                    Price_String = price.ToString().GetPersianNumber(),
+                    TransactionNumber = GetLastTransactionNumber(),
+                    TransactionType = TransactionType.PayFromCreditCard
+                };
+
+                _context.FinancialTransactions.Add(financialTransaction);
+
+                _context.SaveChanges();
+
+                transaction.Commit();
+
+                return new ResultDto<RedirectToPaymentForReservationDto>()
+                {
+                    Data = new RedirectToPaymentForReservationDto()
+                    {
+                        price = Convert.ToInt32(financialTransaction.Price_Digit),
+                        transactionNumber = financialTransaction.TransactionNumber
+                    },
+                    IsSuccess = true,
+                    Message = string.Empty
+                };
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+
+                return new ResultDto<RedirectToPaymentForReservationDto>()
+                {
+                    Data = null,
+                    Message = "خطا!!"
+                };
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+        private string GetLastTransactionNumber()
+        {
+            var financialTransactions = _context.FinancialTransactions.OrderBy(f => f.Id).LastOrDefault();
+
+            if (financialTransactions == null)
+            {
+                return "1";
+            }
+
+
+            return Convert.ToInt32(financialTransactions.TransactionNumber + 1).ToString();
+        }
+    }
+}
