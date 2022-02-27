497e45b0d6742a8a20ea85c9317360af91f3755e
Parsa <Mehdipourparsa@gmail.com>
2022-02-27 12:59:16 +0330

    پیاده سازی سرویس آوردن جزییات فاکتور برای تایید پرداخت 1400/12/08

---
+++ CR.Core/Services/Interfaces/FinancialTransaction/IGetFinancialTransactionDetailsForVerifyService.cs
@@ -0,0 +1,11 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.Factors;
+
+namespace CR.Core.Services.Interfaces.FinancialTransaction
+{
+    public interface IGetFinancialTransactionDetailsForVerifyService
+    {
+        ResultDto<FactorDetailsForVerifyDto> Execute(string transactionNumber);
+
+    }
+}
