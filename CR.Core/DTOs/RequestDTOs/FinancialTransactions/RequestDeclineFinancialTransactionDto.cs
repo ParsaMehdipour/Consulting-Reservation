18148b3bf78a3099e33bb983fec53e99903ff156
Parsa <Mehdipourparsa@gmail.com>
2022-03-02 11:58:09 +0330

    پیاده سازی سرویس لغو تراکنش 1400/12/11

---
+++ CR.Core/DTOs/RequestDTOs/FinancialTransactions/RequestDeclineFinancialTransactionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CR.Core.DTOs.RequestDTOs.FinancialTransactions
+{
+    public class RequestDeclineFinancialTransactionDto
+    {
+        public long factorId { get; set; }
+        public long payerId { get; set; }
+        public int price { get; set; }
+    }
+}
---
+++ CR.Core/Services/Implementations/FinancialTransactions/AddDeclineFinancialTransactionService.cs
@@ -0,0 +1,111 @@
+﻿using CR.Common.Convertor;
+using CR.Common.DTOs;
+using CR.Common.Utilities;
+using CR.Core.Services.Interfaces.FinancialTransaction;
+using CR.DataAccess.Context;
+using CR.DataAccess.Entities.FinancialTransactions;
+using CR.DataAccess.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace CR.Core.Services.Implementations.FinancialTransactions
+{
+    public class AddDeclineFinancialTransactionService : IAddDeclineFinancialTransactionService
+    {
+        private readonly ApplicationContext _context;
+
+        public AddDeclineFinancialTransactionService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto Execute(long payerId, int price, long factorId)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                if (payerId == 0 || price == 0 || factorId == 0)
+                {
+                    return new ResultDto()
+                    {
+                        IsSuccess = false,
+                        Message = "یکی از پارامتر های وروردی صحیح نسیت"
+                    };
+                }
+
+                var factor = _context.Factors
+                    .Include(_ => _.Appointments)
+                    .ThenInclude(_ => _.TimeOfDay)
+                    .FirstOrDefault(_ => _.Id == factorId);
+
+                if (factor == null)
+                {
+                    return new ResultDto()
+                    {
+                        IsSuccess = false,
+                        Message = "فاکتور یافت نشد!!"
+                    };
+                }
+
+                factor.FactorStatus = FactorStatus.Declined;
+
+                foreach (var appointment in factor.Appointments)
+                {
+                    appointment.TimeOfDay.IsReserved = false;
+                }
+
+                var financialTransaction = new FinancialTransaction()
+                {
+                    PayerId = payerId,
+                    Price_Digit = price,
+                    CreateDate_String = DateTime.Now.ToShamsi(),
+                    Price_String = price.ToString().GetPersianNumber(),
+                    TransactionNumber = GetLastTransactionNumber(),
+                    TransactionType = TransactionType.DeclineTransaction,
+                    Status = TransactionStatus.Successful
+                };
+
+                _context.FinancialTransactions.Add(financialTransaction);
+
+                _context.SaveChanges();
+
+                transaction.Commit();
+
+                return new ResultDto()
+                {
+                    IsSuccess = true,
+                    Message = string.Empty
+                };
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+
+                return new ResultDto()
+                {
+                    IsSuccess = false,
+                    Message = "خطا از سمت سرور!!"
+                };
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+
+        private string GetLastTransactionNumber()
+        {
+            var financialTransactions = _context.FinancialTransactions.OrderBy(f => f.Id).LastOrDefault();
+
+            if (financialTransactions == null)
+            {
+                return "1";
+            }
+
+
+            return (Convert.ToInt32(financialTransactions.TransactionNumber) + 1).ToString();
+        }
+    }
+}
