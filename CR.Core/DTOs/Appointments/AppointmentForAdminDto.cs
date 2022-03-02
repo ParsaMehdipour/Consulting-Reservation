0c9f8dc5991590f123e0709c8c64897b4d13ffc3
Parsa <Mehdipourparsa@gmail.com>
2022-03-02 11:50:09 +0330

    تغیر در سرویس نمایش نوبت های پزشک در پنل ادمین و اوردن فاکتور همراه آن 1400/12/11

--- CR.Core/DTOs/Appointments/AppointmentForAdminDto.cs
+++ CR.Core/DTOs/Appointments/AppointmentForAdminDto.cs
@@ -3,13 +3,17 @@
     public class AppointmentForAdminDto
     {
         public long Id { get; set; }
+        public long ExpertId { get; set; }
         public string ExpertIconSrc { get; set; }
         public string ExpertFullName { get; set; }
         public string Speciality { get; set; }
+        public long ConsumerId { get; set; }
         public string ConsumerFullName { get; set; }
         public string ConsumerIconSrc { get; set; }
         public string AppointmentDate { get; set; }
         public string AppointmentTime { get; set; }
         public string AppointmentPrice { get; set; }
+        public long FactorId { get; set; }
+        public string FactorStatus { get; set; }
     }
 }
--- CR.Core/Services/Implementations/Appointment/GetAllAppointmentsForAdminPanelService.cs
+++ CR.Core/Services/Implementations/Appointment/GetAllAppointmentsForAdminPanelService.cs
@@ -1,11 +1,11 @@
-﻿using System.Linq;
-using CR.Common.DTOs;
+﻿using CR.Common.DTOs;
 using CR.Common.Utilities;
 using CR.Core.DTOs.Appointments;
 using CR.Core.DTOs.ResultDTOs.Appointments;
 using CR.Core.Services.Interfaces.Appointment;
 using CR.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CR.Core.Services.Implementations.Appointment
 {
@@ -23,18 +23,23 @@ namespace CR.Core.Services.Implementations.Appointment
             int rowCount = 0;
 
             var appointments = _context.Appointments
+                .Include(a => a.Factor)
                 .Include(a => a.ExpertInformation)
                 .ThenInclude(e => e.Specialty)
                 .Include(a => a.ConsumerInformation)
                 .Include(a => a.TimeOfDay)
                 .ThenInclude(d => d.Day)
-                .OrderByDescending(a=>a.TimeOfDay.Day.Date)
+                .OrderByDescending(a => a.TimeOfDay.Day.Date)
                 .Select(a => new AppointmentForAdminDto
                 {
                     AppointmentDate = a.TimeOfDay.Day.Date_String,
-                    //AppointmentPrice = a.TimeOfDay.Price.ToString().GetPersianNumber(),
+                    AppointmentPrice = a.Price.Value.ToString(),
+                    FactorStatus = a.Factor.FactorStatus.GetDisplayName(),
+                    FactorId = a.FactorId.Value,
+                    ConsumerId = a.ConsumerInformation.ConsumerId,
                     Speciality = a.ExpertInformation.Specialty.Name,
                     ConsumerFullName = a.ConsumerInformation.FirstName + " " + a.ConsumerInformation.LastName,
+                    ExpertId = a.ExpertInformation.ExpertId,
                     ExpertFullName = a.ExpertInformation.FirstName + " " + a.ExpertInformation.LastName,
                     ConsumerIconSrc = a.ConsumerInformation.IconSrc ?? "assets/img/icon-256x256.png",
                     ExpertIconSrc = a.ExpertInformation.IconSrc ?? "assets/img/icon-256x256.png",
