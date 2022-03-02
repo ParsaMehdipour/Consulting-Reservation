f880695ff143a670d499895cba3475f4927e7a3b
Parsa <Mehdipourparsa@gmail.com>
2022-03-02 11:55:34 +0330

    پیاده سازی سرویس نمایش موجودی کیف پول در صفحه رزرو و افزودن قابلیت انتخاب نوع پرداخت 1400/12/11

--- CR.Core/Services/Implementations/Wallet/GetWalletBalanceService.cs
+++ CR.Core/Services/Implementations/Wallet/GetWalletBalanceService.cs
@@ -19,11 +19,11 @@ namespace CR.Core.Services.Implementations.Wallet
         public ResultDto<int> Execute(long payerId)
         {
             var sum = _context.FinancialTransactions
-                .Where(_ => _.TransactionType == TransactionType.ChargeWallet && _.PayerId == payerId)
+                .Where(_ => _.PayerId == payerId && (_.TransactionType == TransactionType.ChargeWallet || _.TransactionType == TransactionType.DeclineTransaction) && _.Status == TransactionStatus.Successful)
                 .Sum(_ => _.Price_Digit);
 
             var minus = _context.FinancialTransactions
-                .Where(_ => _.TransactionType == TransactionType.PayFromWallet && _.PayerId == payerId)
+                .Where(_ => _.PayerId == payerId && _.TransactionType == TransactionType.PayFromWallet && _.Status == TransactionStatus.Successful)
                 .Sum(_ => _.Price_Digit);
 
             var balance = Convert.ToInt32(sum - minus);
