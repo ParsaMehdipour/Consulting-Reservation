8a760bfc5a96b9d5e2712b821e16fc885b8e3526
Parsa <Mehdipourparsa@gmail.com>
2022-03-05 15:52:21 +0330

    افزودن فایل های signalR 1400/12/14

---
+++ CR.Presentation/wwwroot/assets/js/SignalRService.js
@@ -0,0 +1,25 @@
+﻿var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chathub")
+    .build();
+
+connection.start();
+
+function Init() {
+    var NewMessageForm = $("#NewMessageForm");
+    NewMessageForm.on("submit", function (e) {
+
+        e.preventDefault();
+        var message = e.target[0].value;
+        e.target[0].value = '';
+        sendMessage(message);
+    });
+
+}
+
+function sendMessage(text) {
+    connection.invoke('SendNewMessage', " بازدید کننده ", text);
+}
+
+$(document).ready(function () {
+    Init();
+});
