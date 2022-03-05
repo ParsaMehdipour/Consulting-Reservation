f58ca9ce5968d0d424514c4b387c758411fa2b01
Parsa <Mehdipourparsa@gmail.com>
2022-03-05 12:43:06 +0330

    پیاده سازی سرویس و نمایش زمانبندی های موجود افزوده شده توسط ادمین برای پنل متخصص 1400/12/11

---
+++ CR.Core/DTOs/Timings/AvailableTimingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CR.Core.DTOs.Timings
+{
+    public class AvailableTimingDto
+    {
+        public bool isShortSpan { get; set; }
+        public bool isMediumSpan { get; set; }
+        public bool isLongSpan { get; set; }
+    }
+}
