def17c329145f934b6ecff8d077ab43699c978ad
Parsa <Mehdipourparsa@gmail.com>
2022-03-06 16:18:55 +0330

    افزودن کلاس hub برای signal r 1400/12/15

---
+++ CR.Presentation/Hubs/SiteChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace CR.Presentation.Hubs
+{
+    public class SiteChatHub : Hub
+    {
+        public async Task SendNewMessage(string Sender, string Message)
+        {
+            await Clients.All.SendAsync("getNewMessage", Sender, Message, DateTime.Now.ToShortDateString());
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var s = Context.ConnectionId;
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
