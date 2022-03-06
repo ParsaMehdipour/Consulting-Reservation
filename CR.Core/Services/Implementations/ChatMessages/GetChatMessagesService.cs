6fcf62f1e77af0fe94c5707f05e6150a8ddcc36d
Parsa <Mehdipourparsa@gmail.com>
2022-03-06 16:16:05 +0330

    پیاده سازی سرویس نمایش پیام ها 1400/12/15

---
+++ CR.Core/Services/Implementations/ChatMessages/GetChatMessagesService.cs
@@ -0,0 +1,62 @@
+﻿using CR.Common.Convertor;
+using CR.Common.DTOs;
+using CR.Core.DTOs.ChatMessages;
+using CR.Core.DTOs.ResultDTOs.ChatMessages;
+using CR.Core.Services.Interfaces.ChatMessages;
+using CR.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CR.Core.Services.Implementations.ChatMessages
+{
+    public class GetChatMessagesService : IGetChatMessagesService
+    {
+        private readonly ApplicationContext _context;
+
+        public GetChatMessagesService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<ResultGetChatMessagesDto> Execute(long chatUserId)
+        {
+            var chatUser = _context.ChatUsers.Include(_ => _.Consumer).FirstOrDefault(_ => _.Id == chatUserId);
+
+            if (chatUser == null)
+            {
+                return new ResultDto<ResultGetChatMessagesDto>()
+                {
+                    Data = null,
+                    IsSuccess = false,
+                    Message = "کاربر یافت نشد!!"
+                };
+            }
+
+            var chatMessages = _context.ChatUserMessages
+                .Include(_ => _.ChatUser)
+                .ThenInclude(_ => _.Consumer)
+                .ThenInclude(_ => _.ExpertInformation)
+                .Where(_ => _.ChatUserId == chatUserId)
+                .Select(_ => new ChatMessageDto()
+                {
+                    consumerIconSrc = _.ChatUser.Consumer.IconSrc,
+                    expertIconSrc = _.ChatUser.ExpertInformation.IconSrc,
+                    message = _.Message,
+                    messageFlag = _.MessageFlag,
+                    messageHour = $"{_.CreateDate.Minute} : {_.CreateDate.Hour} - {_.CreateDate.ToShamsi()}"
+                }).ToList();
+
+            return new ResultDto<ResultGetChatMessagesDto>()
+            {
+                Data = new ResultGetChatMessagesDto()
+                {
+                    chatMessageDtos = chatMessages,
+                    consumerFullName = chatUser.Consumer.FirstName + " " + chatUser.Consumer.LastName,
+                    consumerIconSrc = chatUser.Consumer.IconSrc
+                },
+                IsSuccess = true
+            };
+
+        }
+    }
+}
