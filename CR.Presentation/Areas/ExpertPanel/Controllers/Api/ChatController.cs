899d99f30b13b80fc940e183e42bb6fb80428d7e
Parsa <Mehdipourparsa@gmail.com>
2022-03-06 16:17:33 +0330

    ایجاد کنترلر های مربوط به پیام های پنل متخصص 1400/12/15

---
+++ CR.Presentation/Areas/ExpertPanel/Controllers/Api/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using CR.Core.DTOs.RequestDTOs.Chat;
+using CR.Core.Services.Interfaces.ChatMessages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.ExpertPanel.Controllers.Api
+{
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IGetChatMessagesService _getChatMessagesService;
+
+        public ChatController(IGetChatMessagesService getChatMessagesService)
+        {
+            _getChatMessagesService = getChatMessagesService;
+        }
+
+        [Route("/api/Chat/GetMessages")]
+        [HttpPost]
+        public IActionResult GetMessages([FromBody] RequestGetChatMessagesDto request)
+        {
+            var result = _getChatMessagesService.Execute(request.chatUserId);
+
+            return new JsonResult(result);
+        }
+    }
+}
