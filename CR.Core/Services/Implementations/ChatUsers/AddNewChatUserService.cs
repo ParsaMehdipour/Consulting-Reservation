8c4b5ae24c1cc5a0e89da6ea7c0abd3e58a4cf26
Parsa <Mehdipourparsa@gmail.com>
2022-03-07 12:05:55 +0330

    پیاده سازی افرودن کانتکت جدید برای پنل مراجعه کننده 1400/12/16

---
+++ CR.Core/Services/Implementations/ChatUsers/AddNewChatUserService.cs
@@ -0,0 +1,55 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.RequestDTOs.ChatUser;
+using CR.Core.Services.Interfaces.ChatUsers;
+using CR.DataAccess.Context;
+using CR.DataAccess.Entities.ChatUsers;
+using System;
+
+namespace CR.Core.Services.Implementations.ChatUsers
+{
+    public class AddNewChatUserService : IAddNewChatUserService
+    {
+        private readonly ApplicationContext _context;
+
+        public AddNewChatUserService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto Execute(RequestAddNewChatUserDto request)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                var chatUser = new ChatUser()
+                {
+                    Consumer = _context.Users.Find(request.consumerId),
+                    ConsumerId = request.consumerId,
+                    ExpertInformation = _context.ExpertInformations.Find(request.expertInformationId),
+                    ExpertInformationId = request.expertInformationId,
+                };
+
+                _context.ChatUsers.Add(chatUser);
+
+                _context.SaveChanges();
+
+                transaction.Commit();
+
+                return new ResultDto()
+                {
+                    IsSuccess = true,
+                    Message = String.Empty
+                };
+            }
+            catch (Exception)
+            {
+                return new ResultDto()
+                {
+                    IsSuccess = false,
+                    Message = "خطا!!"
+                };
+            }
+        }
+    }
+}
---
+++ CR.Core/Services/Interfaces/ChatUsers/IAddNewChatUserService.cs
@@ -0,0 +1,10 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.RequestDTOs.ChatUser;
+
+namespace CR.Core.Services.Interfaces.ChatUsers
+{
+    public interface IAddNewChatUserService
+    {
+        ResultDto Execute(RequestAddNewChatUserDto request);
+    }
+}
