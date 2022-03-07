9988320a6839f24abab3ccac47a5a20250b8deef
Parsa <Mehdipourparsa@gmail.com>
2022-03-07 12:07:37 +0330

    پیاده سازی کنترلر های مربوط به ارسال پیام در پنل مراجعه کننده 1400/12/16

---
+++ CR.Presentation/Areas/ConsumerPanel/Controllers/Api/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using CR.Core.DTOs.RequestDTOs.Chat;
+using CR.Core.Services.Interfaces.ChatMessages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.ConsumerPanel.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IGetChatMessagesService _getChatMessagesService;
+
+        public ChatController(IGetChatMessagesService getChatMessagesService)
+        {
+            _getChatMessagesService = getChatMessagesService;
+        }
+
+        [Route("/api/Chat/GetConsumerMessages")]
+        [HttpPost]
+        public IActionResult GetMessages([FromBody] RequestGetChatMessagesDto request)
+        {
+            var result = _getChatMessagesService.Execute(request.chatUserId, false);
+
+            return new JsonResult(result);
+        }
+    }
+}
