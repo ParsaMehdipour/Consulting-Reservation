ce0d913da0624e88279f4da00b0f3c04d6e01708
Parsa <Mehdipourparsa@gmail.com>
2022-03-13 16:12:55 +0330

    پیاده سازی سرویس پاسخ به نظرات متخصص توسط خود متخصص 1400/12/22

---
+++ CR.Core/Services/Implementations/Comments/AddNewReplyService.cs
@@ -0,0 +1,91 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.RequestDTOs.Comments;
+using CR.Core.Services.Interfaces.Comments;
+using CR.DataAccess.Context;
+using CR.DataAccess.Entities.Comments;
+using CR.DataAccess.Enums;
+using System;
+using System.Linq;
+
+namespace CR.Core.Services.Implementations.Comments
+{
+    public class AddNewReplyService : IAddNewReplyService
+    {
+        private readonly ApplicationContext _context;
+
+        public AddNewReplyService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto Execute(RequestAddNewReplyDto request, long userId)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                var expertInformation = _context.ExpertInformations.FirstOrDefault(_ => _.ExpertId == userId);
+
+                if (expertInformation == null)
+                {
+                    return new ResultDto()
+                    {
+                        IsSuccess = false,
+                        Message = "اطلاعات شما یافت نشد!!"
+                    };
+                }
+
+                var comment = new Comment()
+                {
+                    UserId = userId,
+                    User = _context.Users.Find(userId),
+                    CommentStatus = CommentStatus.Waiting,
+                    IsRead = false,
+                    Message = request.message,
+                    OwnerRecordId = expertInformation.Id,
+                    TypeId = request.typeId,
+                };
+
+                _context.Comments.Add(comment);
+
+                var parentComment = GetParentComment(request.parentId);
+
+                comment.Parent = parentComment;
+                comment.ParentId = request.parentId;
+
+                _context.SaveChanges();
+
+                parentComment.Children.Add(comment);
+
+
+                _context.SaveChanges();
+
+                transaction.Commit();
+
+                return new ResultDto()
+                {
+                    IsSuccess = true,
+                    Message = "نظر شما با موفقیت ثبت شد"
+                };
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+
+                return new ResultDto()
+                {
+                    IsSuccess = false,
+                    Message = "خطا!!"
+                };
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+        private Comment GetParentComment(long parentId)
+        {
+            return _context.Comments.Find(parentId);
+        }
+    }
+}
---
+++ CR.Core/Services/Interfaces/Comments/IAddNewReplyService.cs
@@ -0,0 +1,10 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.RequestDTOs.Comments;
+
+namespace CR.Core.Services.Interfaces.Comments
+{
+    public interface IAddNewReplyService
+    {
+        ResultDto Execute(RequestAddNewReplyDto request, long userId);
+    }
+}
