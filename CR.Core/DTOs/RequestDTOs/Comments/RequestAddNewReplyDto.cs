a04b37217413ab000a60e693a34ba2290623fb4e
Parsa <Mehdipourparsa@gmail.com>
2022-03-13 16:12:26 +0330

    پیاده سازی سرویس و نمایش نظرات مربوط به هر متخصص در پنل او 1400/12/22

---
+++ CR.Core/DTOs/RequestDTOs/Comments/RequestAddNewReplyDto.cs
@@ -0,0 +1,11 @@
+﻿using CR.DataAccess.Enums;
+
+namespace CR.Core.DTOs.RequestDTOs.Comments
+{
+    public class RequestAddNewReplyDto
+    {
+        public string message { get; set; }
+        public CommentType typeId { get; set; }
+        public long parentId { get; set; }
+    }
+}
---
+++ CR.Presentation/Areas/ExpertPanel/Controllers/View/CommentsController.cs
@@ -0,0 +1,28 @@
+﻿using CR.Common.Utilities;
+using CR.Core.Services.Interfaces.Comments;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.ExpertPanel.Controllers.View
+{
+    [Area("ExpertPanel")]
+    [Authorize]
+    public class CommentsController : Controller
+    {
+        private readonly IGetExpertCommentsForExpertPanelService _getExpertCommentsForExpertPanelService;
+
+        public CommentsController(IGetExpertCommentsForExpertPanelService getExpertCommentsForExpertPanelService)
+        {
+            _getExpertCommentsForExpertPanelService = getExpertCommentsForExpertPanelService;
+        }
+
+        public IActionResult Index(int page = 1, int pageSize = 20)
+        {
+            var expertId = ClaimUtility.GetUserId(User).Value;
+
+            var model = _getExpertCommentsForExpertPanelService.Execute(expertId, page, pageSize).Data;
+
+            return View(model);
+        }
+    }
+}
