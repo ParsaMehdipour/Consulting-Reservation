99013490f386372c4643c73c7b6255c0be74fad2
Parsa <Mehdipourparsa@gmail.com>
2022-03-26 14:44:35 +0430

    پیاده سازی سرویس افزودن شارژ کیف پول متخصص 1401/01/06

---
+++ CR.Core/Services/Interfaces/FinancialTransaction/IAddChargeExpertWalletService.cs
@@ -0,0 +1,9 @@
+﻿using CR.Common.DTOs;
+
+namespace CR.Core.Services.Interfaces.FinancialTransaction
+{
+    public interface IAddChargeExpertWalletService
+    {
+        ResultDto Execute(long receiverId, int price);
+    }
+}
