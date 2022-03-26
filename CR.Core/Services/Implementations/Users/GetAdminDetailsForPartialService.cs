9a2e735bd368e006ec76b9545f997147e400dabc
Parsa <Mehdipourparsa@gmail.com>
2022-03-26 14:50:17 +0430

    پیاده سازی سرویس گرفتن اطلاعات مدیریت برای منوی پارشیال 1401/01/06

---
+++ CR.Core/Services/Implementations/Users/GetAdminDetailsForPartialService.cs
@@ -0,0 +1,45 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.Users;
+using CR.Core.Services.Interfaces.Users;
+using CR.DataAccess.Context;
+using CR.DataAccess.Enums;
+using System.Linq;
+
+namespace CR.Core.Services.Implementations.Users
+{
+    public class GetAdminDetailsForPartialService : IGetAdminDetailsForPartialService
+    {
+        private readonly ApplicationContext _context;
+
+        public GetAdminDetailsForPartialService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<AdminForPartialDto> Execute(long userId)
+        {
+            var admin = _context.Users
+                .Where(u => u.UserFlag == UserFlag.Admin)
+                .FirstOrDefault(c => c.Id == userId);
+
+            if (admin == null)
+            {
+                return new ResultDto<AdminForPartialDto>()
+                {
+                    IsSuccess = false,
+                    Message = "اطلاعات شما یافت نشد"
+                };
+            }
+
+            return new ResultDto<AdminForPartialDto>()
+            {
+                Data = new AdminForPartialDto()
+                {
+                    fullName = admin.FirstName + " " + admin.LastName,
+                    iconSrc = admin.IconSrc ?? "assets/img/User.png"
+                },
+                IsSuccess = true
+            };
+        }
+    }
+}
---
+++ CR.Presentation/Areas/AdminPanel/Controllers/Api/AdminDetailsController.cs
@@ -0,0 +1,28 @@
+﻿using CR.Common.DTOs;
+using CR.Common.Utilities;
+using CR.Core.DTOs.Users;
+using CR.Core.Services.Interfaces.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers.Api
+{
+    [ApiController]
+    public class AdminDetailsController : ControllerBase
+    {
+        private readonly IGetAdminDetailsForPartialService _getAdminDetailsForPartialService;
+
+        public AdminDetailsController(IGetAdminDetailsForPartialService getAdminDetailsForPartialService)
+        {
+            _getAdminDetailsForPartialService = getAdminDetailsForPartialService;
+        }
+
+        [Route("/api/AdminDetails/GetDetails")]
+        [HttpGet]
+        public ResultDto<AdminForPartialDto> GetDetails()
+        {
+            var userId = ClaimUtility.GetUserId(User).Value;
+
+            return _getAdminDetailsForPartialService.Execute(userId);
+        }
+    }
+}
