5030211118ab9be1a52f59d71579eddc66aff479
Parsa <Mehdipourparsa@gmail.com>
2022-03-28 11:01:37 +0430

    پیاده سازی سرویس افزودن درخواست تسویه حساب از سمت مشاور 1401/01/08

---
+++ CR.Presentation/Areas/ExpertPanel/Controllers/Api/FinancialTransactionsController.cs
@@ -0,0 +1,29 @@
+﻿using CR.Common.Utilities;
+using CR.Core.DTOs.RequestDTOs.FinancialTransactions;
+using CR.Core.Services.Interfaces.FinancialTransaction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.ExpertPanel.Controllers.Api
+{
+    [ApiController]
+    public class FinancialTransactionsController : ControllerBase
+    {
+        private readonly IAddCheckoutFinancialTransactionService _addCheckoutFinancialTransactionService;
+
+        public FinancialTransactionsController(IAddCheckoutFinancialTransactionService addCheckoutFinancialTransactionService)
+        {
+            _addCheckoutFinancialTransactionService = addCheckoutFinancialTransactionService;
+        }
+
+        [Route("/api/FinancialTransactions/RequestCheckout")]
+        [HttpPost]
+        public IActionResult RequestCheckout([FromForm] RequestCheckoutDto model)
+        {
+            var receiverId = ClaimUtility.GetUserId(User).Value;
+
+            var result = _addCheckoutFinancialTransactionService.Execute(receiverId, model.price);
+
+            return new JsonResult(result);
+        }
+    }
+}
