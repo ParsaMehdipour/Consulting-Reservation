d8a587895329b07c0fdfb828500befd45e43582b
Parsa <Mehdipourparsa@gmail.com>
2022-03-30 14:25:10 +0430

    پیاده سازی سرویس ستاره دهی در نظرات 1401/01/10

--- CR.Core/DTOs/RequestDTOs/Comments/RequestAddNewCommentDto.cs
+++ CR.Core/DTOs/RequestDTOs/Comments/RequestAddNewCommentDto.cs
@@ -8,5 +8,6 @@ namespace CR.Core.DTOs.RequestDTOs.Comments
         public CommentType typeId { get; set; }
         public long ownerRecordId { get; set; }
         public long? parentId { get; set; }
+        public int? rate { get; set; }
     }
 }
