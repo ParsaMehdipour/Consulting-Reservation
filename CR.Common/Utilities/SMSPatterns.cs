d113bee130365e6914f0fbd316e1a03a9e43cbdf
Parsa <Mehdipourparsa@gmail.com>
2022-04-04 13:27:31 +0430

    اعمال تغیرات در کانفیگ مدل پنل پیامکی 1401/01/15

--- CR.Common/Utilities/SMSPatterns.cs
+++ CR.Common/Utilities/SMSPatterns.cs
@@ -4,7 +4,7 @@
     {
         public static string ReservationNotDone_ExpertSide { get; set; } = "wnprvnvez7";
         public static string ReservationDeclined_ExpertSide { get; set; } = "is0646usdp";
-        public static string ReservationSuccessfulPatternCode_Expert_Side { get; set; } = "4d37ozxno6";
+        public static string ReservationSuccessfulPatternCode_ExpertSide { get; set; } = "4d37ozxno6";
         public static string ReservationSuccessfulPatternCode_ConsumerSide { get; set; } = "9szh3m66d5";
         public static string ReservationDeclined_ConsumerSide { get; set; } = "yeffx8deb5";
         public static string ResetPasswordPatternCode { get; set; } = "7qydwvcb94";
