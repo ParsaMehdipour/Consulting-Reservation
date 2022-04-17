b312a9f758c845083a2e9ca05bed5518a507ed3b
Parsa <Mehdipourparsa@gmail.com>
2022-04-17 14:24:13 +0430

    پیاده سازی سرویس های مربوط به تنظیمات سایت 1401/01/28

---
+++ CR.Core/Services/Interfaces/Settings/ISettingServices.cs
@@ -0,0 +1,13 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.RequestDTOs.Settings;
+using CR.Core.DTOs.Settings;
+
+namespace CR.Core.Services.Interfaces.Settings
+{
+    public interface ISettingServices
+    {
+        ResultDto Add(AddSettingDto request);
+        ResultDto Edit(EditSettingDto request);
+        ResultDto<SettingDto> Get();
+    }
+}
