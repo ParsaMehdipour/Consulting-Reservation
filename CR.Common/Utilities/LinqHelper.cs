3c27bb24c6c9a6f76623fd6d665430b0f8083736
Parsa <Mehdipourparsa@gmail.com>
2022-04-17 11:18:36 +0430

    تغیر در شیوه چیدمان مشاورین 1401/01/28

---
+++ CR.Common/Utilities/LinqHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CR.Common.Utilities
+{
+    public static class LinqHelper
+    {
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> data)
+        {
+            List<T> listResult = new List<T>();
+            List<T> lList = data.ToList();
+            Random lRandom = new();
+
+            while (lList.Count > 0)
+            {
+                var lintPos = lRandom.Next(lList.Count);
+                listResult.Add(lList[lintPos]);
+                lList.RemoveAt(lintPos);
+            }
+
+            return listResult;
+        }
+    }
+}
