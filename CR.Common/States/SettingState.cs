3c7296e4ae90a5afa421f195dae784c26c86d8a6
Parsa <Mehdipourparsa@gmail.com>
2022-04-17 14:25:46 +0430

    پیاده سازی مدل جهت بروزرسانی تنظیمات سایت 1401/01/28

---
+++ CR.Common/States/SettingState.cs
@@ -0,0 +1,9 @@
+﻿namespace CR.Common.States
+{
+    public static class SettingState
+    {
+        public static string SiteTitle { get; set; }
+        public static string SiteLogo { get; set; }
+        public static string SiteFavicon { get; set; }
+    }
+}
