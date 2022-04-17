798e9893b132cb55e9c49ae2deacba35ddfba67f
Parsa <Mehdipourparsa@gmail.com>
2022-04-17 14:26:19 +0430

    پیاده سازی مدل های تنظیمات سایت جهت افزودن و ویرایش تنظیمات 1401/01/28

---
+++ CR.Core/DTOs/Settings/SettingDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CR.Core.DTOs.Settings
+{
+    public class SettingDto
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "لطفا عنوان سایت را وارد کنید")]
+        public string Title { get; set; }
+        public string FavIcon { get; set; }
+        public string Logo { get; set; }
+    }
+}
