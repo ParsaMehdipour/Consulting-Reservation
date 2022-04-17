c735999b574da90ef1e62e3aa21a33252d2b4727
Parsa <Mehdipourparsa@gmail.com>
2022-04-17 14:23:42 +0430

    افزودن مدل مربوط به تنظیمات سایت 1401/01/28

---
+++ CR.DataAccess/Entities/Settings/Setting.cs
@@ -0,0 +1,11 @@
+﻿using CR.DataAccess.Common.Entity;
+
+namespace CR.DataAccess.Entities.Settings
+{
+    public class Setting : BaseEntity
+    {
+        public string Title { get; set; }
+        public string FavIcon { get; set; }
+        public string Logo { get; set; }
+    }
+}
