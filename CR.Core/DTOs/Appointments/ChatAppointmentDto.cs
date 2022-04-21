5bc1fed2128c6312478fe5df5ff3542ad1f664f3
Parsa <Mehdipourparsa@gmail.com>
2022-04-21 15:09:47 +0430

    اعمال تغیر در مدل های خرید نوبت 1401/02/01

--- CR.Core/DTOs/Appointments/ChatAppointmentDto.cs
+++ CR.Core/DTOs/Appointments/ChatAppointmentDto.cs
@@ -5,6 +5,7 @@ namespace CR.Core.DTOs.Appointments
 {
     public class ChatAppointmentDto
     {
+        public long Id { get; set; }
         public CallingType CallingType { get; set; }
         public DateTime AppointmentDate { get; set; }
     }
