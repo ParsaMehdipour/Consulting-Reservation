3dda0544aabbf68b7d9515c1a60a22b4901ea563
Parsa <Mehdipourparsa@gmail.com>
2022-04-26 20:45:16 +0430

    افزودن کلاس مربوط به نمایش ، افزودن لینک 1401/02/06

---
+++ CR.Core/DTOs/ResultDTOs/Links/ResultGetLinksForAdminPanelDto.cs
@@ -0,0 +1,13 @@
+﻿using CR.Core.DTOs.Links;
+using System.Collections.Generic;
+
+namespace CR.Core.DTOs.ResultDTOs.Links
+{
+    public class ResultGetLinksForAdminPanelDto
+    {
+        public int RowCount { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public List<LinkForAdminPanelDto> LinkForAdminPanelDtos { get; set; }
+    }
+}
