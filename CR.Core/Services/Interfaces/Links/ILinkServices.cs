78873767d1a743fee1c218c28d57dcd9ea747c46
Parsa <Mehdipourparsa@gmail.com>
2022-04-26 20:40:32 +0430

    افزودن سرویس مربوط به لینک 1402/02/06

---
+++ CR.Core/Services/Interfaces/Links/ILinkServices.cs
@@ -0,0 +1,12 @@
+﻿using CR.Common.DTOs;
+using CR.Core.DTOs.RequestDTOs.Links;
+using CR.Core.DTOs.ResultDTOs.Links;
+
+namespace CR.Core.Services.Interfaces.Links
+{
+    public interface ILinkServices
+    {
+        ResultDto<ResultGetLinksForAdminPanelDto> GetAllLinksForAdminPanel(string searchKey, int Page = 1, int PageSize = 20);
+        ResultDto AddNewLink(RequestAddNewLinkDto request);
+    }
+}
