151adfaeddb8a9fce8bb44763aa1b013bbee62ec
Parsa <Mehdipourparsa@gmail.com>
2022-04-26 20:40:01 +0430

    migrations 1401/02/06

---
+++ CR.DataAccess/Migrations/20220425061647_AddedLink.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class AddedLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Link",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersianTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    SearchKey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrderNumber = table.Column<int>(type: "int", nullable: false),
+                    ParentLinkId = table.Column<long>(type: "bigint", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Link", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Link_Link_ParentLinkId",
+                        column: x => x.ParentLinkId,
+                        principalTable: "Link",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Link_ParentLinkId",
+                table: "Link",
+                column: "ParentLinkId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Link");
+        }
+    }
+}
---
+++ CR.DataAccess/Migrations/20220426142137_ChangedLinkRelation.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class ChangedLinkRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Link_Link_ParentLinkId",
+                table: "Link");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Link",
+                table: "Link");
+
+            migrationBuilder.RenameTable(
+                name: "Link",
+                newName: "Links");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Link_ParentLinkId",
+                table: "Links",
+                newName: "IX_Links_ParentLinkId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Links",
+                table: "Links",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Links_Links_ParentLinkId",
+                table: "Links",
+                column: "ParentLinkId",
+                principalTable: "Links",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Links_Links_ParentLinkId",
+                table: "Links");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Links",
+                table: "Links");
+
+            migrationBuilder.RenameTable(
+                name: "Links",
+                newName: "Link");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Links_ParentLinkId",
+                table: "Link",
+                newName: "IX_Link_ParentLinkId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Link",
+                table: "Link",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Link_Link_ParentLinkId",
+                table: "Link",
+                column: "ParentLinkId",
+                principalTable: "Link",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
