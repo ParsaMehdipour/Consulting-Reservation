2f93109ca15c8fb90fdb48f34f731e30aee88153
Parsa <Mehdipourparsa@gmail.com>
2022-04-26 20:44:34 +0430

    افزودن کنترلر مربوط به لینک ها 1401/02/06

---
+++ CR.Presentation/Areas/AdminPanel/Controllers/Api/LinksController.cs
@@ -0,0 +1,26 @@
+﻿using CR.Core.DTOs.RequestDTOs.Links;
+using CR.Core.Services.Interfaces.Links;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers.Api
+{
+    [ApiController]
+    public class LinksController : ControllerBase
+    {
+        private readonly ILinkServices _linkServices;
+
+        public LinksController(ILinkServices linkServices)
+        {
+            _linkServices = linkServices;
+        }
+
+        [HttpPost]
+        [Route("/api/LinksController/AddNewLink")]
+        public IActionResult AddNewLink([FromForm] RequestAddNewLinkDto request)
+        {
+            var result = _linkServices.AddNewLink(request);
+
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ CR.Presentation/Areas/AdminPanel/Controllers/View/LinksController.cs
@@ -0,0 +1,37 @@
+﻿using CR.Common.ActiveMenus;
+using CR.Core.Services.Interfaces.Links;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR.Presentation.Areas.AdminPanel.Controllers.View
+{
+    [Area("AdminPanel")]
+    public class LinksController : Controller
+    {
+        private readonly ILinkServices _linkServices;
+
+        public LinksController(ILinkServices linkServices)
+        {
+            _linkServices = linkServices;
+        }
+
+        public IActionResult Index(string searchKey, int Page = 1, int PageSize = 20)
+        {
+            TempData["activemenu"] = ActiveMenu.Links;
+
+            var model = _linkServices.GetAllLinksForAdminPanel(searchKey, Page, PageSize).Data;
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create(long? parentId)
+        {
+            TempData["activemenu"] = ActiveMenu.Links;
+
+            ViewBag.parentId = parentId;
+
+            return View();
+        }
+
+    }
+}
