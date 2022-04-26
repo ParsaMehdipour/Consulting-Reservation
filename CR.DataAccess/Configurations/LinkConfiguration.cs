37d32e8e0448d4ed4416a92426321f88fecc2887
Parsa <Mehdipourparsa@gmail.com>
2022-04-26 20:43:28 +0430

    افزودن کانفیگ مدل لینک 1401/02/06

---
+++ CR.DataAccess/Configurations/LinkConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CR.DataAccess.Entities.Links;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CR.DataAccess.Configurations
+{
+    public class LinkConfiguration : IEntityTypeConfiguration<Link>
+    {
+        public void Configure(EntityTypeBuilder<Link> builder)
+        {
+            builder.Property(_ => _.PersianTitle).HasMaxLength(100).IsRequired();
+
+            builder.HasOne(_ => _.Parent)
+                .WithMany(_ => _.Children)
+                .HasForeignKey(_ => _.ParentLinkId);
+        }
+    }
+}
