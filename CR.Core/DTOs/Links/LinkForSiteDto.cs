276c76c510a0b9af19c315aef34f82e9f1482ca1
Parsa <Mehdipourparsa@gmail.com>
2022-05-05 11:51:51 +0430

    پیاده سازی سرویس نمایش لینک ها در صفحه اصلی سایت 1401/02/15

---
+++ CR.Core/DTOs/Links/LinkForSiteDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CR.Core.DTOs.Links
+{
+    public class LinkForSiteDto
+    {
+        public string Name { get; set; }
+        public string SearchKey { get; set; }
+        public ICollection<LinkForSiteDto> Children { get; set; }
+    }
+}
