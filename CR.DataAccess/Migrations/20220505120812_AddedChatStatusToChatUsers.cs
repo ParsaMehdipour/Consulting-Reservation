53b598e21980b1116eef28fa6161c3a97ff7f4c5
Parsa <Mehdipourparsa@gmail.com>
2022-05-05 17:33:04 +0430

    migrations 1401/02/15

---
+++ CR.DataAccess/Migrations/20220505120812_AddedChatStatusToChatUsers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class AddedChatStatusToChatUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChatStatus",
+                table: "TBL_ChatUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatStatus",
+                table: "TBL_ChatUsers");
+        }
+    }
+}
