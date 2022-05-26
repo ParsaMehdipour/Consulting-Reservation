0a467f665d4c72eb702f8b3189e3c820df6f8af3
chante.soft <chante.soft@gmail.com>
2022-05-26 18:48:19 +0430

    افزودن rowversion , اپدیت sitesetting 1401/03/05

---
+++ CR.DataAccess/Migrations/20220526141646_AddRowVersio_UpdateSiteSetting.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class AddRowVersio_UpdateSiteSetting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExpertVector",
+                table: "TBL_Settings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserVector",
+                table: "TBL_Settings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "TBL_ExpertInformations",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "TBL_ConsumersInformations",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "AspNetUsers",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpertVector",
+                table: "TBL_Settings");
+
+            migrationBuilder.DropColumn(
+                name: "UserVector",
+                table: "TBL_Settings");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "TBL_ExpertInformations");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "TBL_ConsumersInformations");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "AspNetUsers");
+        }
+    }
+}
