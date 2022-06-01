6dcdc110ce4e87fa945982e7f29f3becdd41cbce
Parsa <Mehdipourparsa@gmail.com>
2022-06-01 15:43:16 +0430

    اعمال تغیر در مدل تخصص

--- CR.DataAccess/Entities/Specialties/Specialty.cs
+++ CR.DataAccess/Entities/Specialties/Specialty.cs
@@ -1,6 +1,6 @@
 ﻿using CR.DataAccess.Common.Entity;
-using System.Collections.Generic;
 using CR.DataAccess.Entities.IndividualInformations;
+using System.Collections.Generic;
 
 namespace CR.DataAccess.Entities.Specialties
 {
@@ -10,10 +10,42 @@ namespace CR.DataAccess.Entities.Specialties
         public string ImageSrc { get; set; }
 
 
-        #region Navigation Properties
+        #region Navigation Properties\
 
+        public long? ParentSpecialtyId { get; set; }
+        public Specialty Parent { get; set; }
         public List<ExpertInformation> ExpertInformations { get; set; }
 
         #endregion
+
+        public Specialty(string name, string imageSrc)
+        {
+            SetName(name);
+            SetImageSrc(imageSrc);
+        }
+
+        public void SetName(string name)
+        {
+            if (Name == name)
+                return;
+
+            Name = name;
+        }
+
+        public void SetImageSrc(string imageSrc)
+        {
+            if (ImageSrc == imageSrc)
+                return;
+
+            ImageSrc = imageSrc;
+        }
+
+        public void SetParent(long parentId)
+        {
+            if (ParentSpecialtyId == parentId)
+                return;
+            else
+                ParentSpecialtyId = parentId;
+        }
     }
 }
