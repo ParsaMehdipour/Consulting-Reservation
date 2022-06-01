9a81304ea04db02d9451ae3d09e71ea8eb642308
Parsa <Mehdipourparsa@gmail.com>
2022-06-01 15:44:19 +0430

    اعمال تغیر در مدل نمایش تخصص در پنل ادمین

--- CR.Core/DTOs/Specialities/SpecialityDto.cs
+++ CR.Core/DTOs/Specialities/SpecialityDto.cs
@@ -3,7 +3,9 @@
     public class SpecialityDto
     {
         public long Id { get; set; }
+        public SpecialityDto Parent { get; set; }
         public string Name { get; set; }
         public string ImageSrc { get; set; }
+        public bool HasChildren { get; set; }
     }
 }
