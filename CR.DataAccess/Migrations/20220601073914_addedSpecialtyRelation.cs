9e7ca2f543bb331fca91a53f6f76acef062fb6f2
Parsa <Mehdipourparsa@gmail.com>
2022-06-01 15:43:25 +0430

    migrations

---
+++ CR.DataAccess/Migrations/20220601073914_addedSpecialtyRelation.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CR.DataAccess.Migrations
+{
+    public partial class addedSpecialtyRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ParentSpecialtyId",
+                table: "TBL_Specialties",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBL_Specialties_ParentSpecialtyId",
+                table: "TBL_Specialties",
+                column: "ParentSpecialtyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TBL_Specialties_TBL_Specialties_ParentSpecialtyId",
+                table: "TBL_Specialties",
+                column: "ParentSpecialtyId",
+                principalTable: "TBL_Specialties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TBL_Specialties_TBL_Specialties_ParentSpecialtyId",
+                table: "TBL_Specialties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TBL_Specialties_ParentSpecialtyId",
+                table: "TBL_Specialties");
+
+            migrationBuilder.DropColumn(
+                name: "ParentSpecialtyId",
+                table: "TBL_Specialties");
+        }
+    }
+}
