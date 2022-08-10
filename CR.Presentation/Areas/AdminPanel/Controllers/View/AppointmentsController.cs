83db6b5bc804b0dc415b1888d487dc8157161ca8
chante.soft <chante.soft@gmail.com>
2022-08-10 15:05:27 +0430

    نمایش صفحات چت مربوط به هر مشاور در پنل مدیریت

--- CR.Presentation/Areas/AdminPanel/Controllers/View/AppointmentsController.cs
+++ CR.Presentation/Areas/AdminPanel/Controllers/View/AppointmentsController.cs
@@ -1,6 +1,7 @@
 ﻿using CR.Common.ActiveMenus;
 using CR.Core.DTOs.Appointments;
 using CR.Core.Services.Interfaces.Appointment;
+using CR.Core.Services.Interfaces.ChatUsers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,12 +13,14 @@ namespace CR.Presentation.Areas.AdminPanel.Controllers.View
     {
         private readonly IGetAllAppointmentsForAdminPanelService _getAllAppointmentsService;
         private readonly IGetAppointmentDetailsForExpertPanelService _getAppointmentDetailsForExpertPanelService;
+        private readonly IGetExpertChatUsersService _getExpertChatUsersService;
 
         public AppointmentsController(IGetAllAppointmentsForAdminPanelService getAllAppointmentsService
-        , IGetAppointmentDetailsForExpertPanelService getAppointmentDetailsForExpertPanelService)
+        , IGetAppointmentDetailsForExpertPanelService getAppointmentDetailsForExpertPanelService, IGetExpertChatUsersService getExpertChatUsersService)
         {
             _getAllAppointmentsService = getAllAppointmentsService;
             _getAppointmentDetailsForExpertPanelService = getAppointmentDetailsForExpertPanelService;
+            _getExpertChatUsersService = getExpertChatUsersService;
         }
 
         public IActionResult Index(int page = 1, int pageSize = 20)
@@ -33,6 +36,15 @@ namespace CR.Presentation.Areas.AdminPanel.Controllers.View
         public AppointmentDetailsForExpertPanel GetDetails(long id)
         {
             return _getAppointmentDetailsForExpertPanelService.Execute(id).Data;
+        }
+
+        public IActionResult ChatDetails(long expertId)
+        {
+            TempData["activemenu"] = ActiveMenu.Appointments;
+
+            var model = _getExpertChatUsersService.Execute(expertId, "").Data;
+
+            return View(model);
         }
     }
 }
